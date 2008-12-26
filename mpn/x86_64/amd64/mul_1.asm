a9c73eb1ff247e6af70f4fec542f6dfeb229003d
jasonmoxham <jason@njkfrudils.plus.com>
2008-12-26 17:57:01 +0000

    added missing license stuff to my files

--- mpn/x86_64/amd64/mul_1.asm
+++ mpn/x86_64/amd64/mul_1.asm
@@ -2,6 +2,23 @@ dnl  AMD64 mpn_mul_1
 
 dnl  Copyright 2008 Jason Moxham
 
+dnl  This file is part of the MPIR Library.
+
+dnl  The MPIR Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The MPIR Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the MPIR Library; see the file COPYING.LIB.  If not, write
+dnl  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+dnl  Boston, MA 02110-1301, USA.
+
 include(`../config.m4')
 
 C	(rdi,rdx)=(rsi,rdx)*rcx
