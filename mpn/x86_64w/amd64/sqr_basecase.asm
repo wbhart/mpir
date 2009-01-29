59d9328adb298509fd3748c1ab6fd8c2e58f9b69
gladman <brg@gladman.plus.com>
2009-01-29 12:24:26 +0000

    Add addadd, addsub and sumdiff assembler for Windows. Update sqr_basecase for Windows Update Windows build files

--- mpn/x86_64w/amd64/sqr_basecase.asm
+++ mpn/x86_64w/amd64/sqr_basecase.asm
@@ -1,16 +1,15 @@
-
 ;  AMD64 mpn_sqr_basecase -- square an mpn number.
-;  Version 1.0.3.
+;  Version 1.0.4.
 ;
-;  Copyright 2009 Jason Moxham
+;  Copyright 2008 Jason Moxham
 
 ;  Windows Conversion CopyRight 2008 Brian Gladman
 
 ;  This file is part of the MPIR Library.
 ;  The MPIR Library is free software; you can redistribute it and/or modify
 ;  it under the terms of the GNU Lesser General Public License as published
-;  by the Free Software Foundation; either version 2.1 of the License, or (at
-;  your option) any later version.
+;  by the Free Software Foundation; either verdxon 2.1 of the License, or (at
+;  your option) any later verdxon.
 ;  The MPIR Library is distributed in the hope that it will be useful, but
 ;  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ;  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
@@ -32,26 +31,15 @@
 ;
 ; This is an SEH frame function with a leaf prologue
 
-; changes from standard mpn_mul_1
-; change lables
-; change r8 to r11
-; write top limb ax straight to mem dont return
-; change lea offsets to match addmul
-; reorder mod4 tail parts so have "only one ret"
-; remove ret
-; todo --------  could ignore small values ie mulskiploop for dx=1,2,3 we save one branch
-; todo ------------- alignment of jumps etc
-
 %macro mpn_mul_1_int 0
 
 	mov     r10d, 4
 	sub     r10, rdx
 	mov     r11d, 0
 	jnc     %%2
-
 	align   16
 
-%%1:	mov     rax, [rsi+r10*8+16]
+%%1:mov     rax, [rsi+r10*8+16]
 	mov     r9d, 0
 	mul     r13
 	add     r11, rax
@@ -94,7 +82,7 @@
 	add     r10, 2
 	mov     [rdi+r10*8+8], r9
 
-%%3:	test    r10, 1
+%%3:test    r10, 1
 	jnz     %%4
 	mov     rax, [rsi+r10*8+16]
 	mov     r9d, 0
@@ -103,7 +91,8 @@
 	adc     r9, rdx
 	mov     [rdi+r10*8+24], r9
 
-%%4:	mov     [rdi+r10*8+16], r11
+%%4:mov     [rdi+r10*8+16], r11
+
 %endmacro
 
 ; changes from standard mpn_addmul_1 internal loop
@@ -257,12 +246,11 @@
 	cmp     r14, 0
 	jge     %%2
 	addmulloop %1
-
-%%2:	addmulnext%1
+%%2:addmulnext%1
 	inc     r14
 	lea     rdi, [rdi+8]
 	cmp     r14, 4
-	jz      .6
+	jz      .7
 
 %endmacro
 
@@ -280,53 +268,98 @@
 %endif
 
 __gmpn_sqr_basecase:
-	cmp     r8d, 1
-	ja      .2
-.1:	mov     rax, [rdx]
+    cmp     r8d, 3
+	jnb     .3
+	jp      .2
+
+.1: mov     rax, [rdx]
 	mul     rax
 	mov     [rcx], rax
 	mov     [rcx+8], rdx
 	ret
 
-.2: prologue __gmpn_sqr_bc, 0, reg_save_list
+.2: mov     rax, [rdx]
+	mov     r9, [rdx+8]
+	mov     r8, rax
+	mul     rax
+	mov     [rcx], rax
+	mov     rax, r9
+	mov     [rcx+8], rdx
+	mul     rax
+	mov     [rcx+16], rax
+	mov     rax, r8
+	mov     r10, rdx
+	mul     r9
+	add     rax, rax
+	adc     rdx, rdx
+	adc     r10, 0
+	add     [rcx+8], rax
+	adc     [rcx+16], rdx
+	adc     r10, 0
+	mov     [rcx+24], r10
+	ret
+
+.3  ja      sqr_main
+    prologue sqr_3, 0, rsi, rdi
+    mov     rsi, rdx
+    mov     r8, [rsi]
+	mov     rax, [rsi+8]
+	mul     r8
+	mov     r11d, 0
+	mov     [rcx+8], rax
+	mov     rax, [rsi+16]
+	mov     r9, rdx
+	mul     r8
+	mov     r8, [rsi+8]
+	add     r9, rax
+	mov     rax, [rsi+16]
+	adc     r11, rdx
+	mul     r8
+	mov     [rcx+16], r9
+	add     r11, rax
+	mov     r9d, 0
+	mov     [rcx+24], r11
+	adc     r9, rdx
+	mov     [rcx+32], r9
+	mov     edi, 3
+	xor     r10, r10
+	xor     r11, r11
+	lea     rsi, [rsi+24]
+	mov     [rcx], r11
+	mov     [rcx+40], r11
+	neg     rdi
+
+.4: mov     rax, [rsi+rdi*8]
+	mul     rax
+	mov     r8, [rcx]
+	mov     r9, [rcx+8]
+	add     r10, 1
+	adc     r8, r8
+	adc     r9, r9
+	sbb     r10, r10
+	add     r11, 1
+	adc     r8, rax
+	adc     r9, rdx
+	sbb     r11, r11
+	mov     [rcx], r8
+	mov     [rcx+8], r9
+	inc     rdi
+	lea     rcx, [rcx+16]
+	jnz     .4
+	epilogue rsi, rdi
+
+; this code can not handle cases 3,2,1
+    prologue sqr_main, 0, reg_save_list
     mov     rdi, rcx
     mov     rsi, rdx
     mov     edx, r8d
-    cmp     rdx, 3
-    ja      .4
-    jz      .3
-    mov     rax, [rsi]
-	mul     qword [rsi+8]
-	mov     [rdi+8], rax
-	mov     [rdi+16], rdx
-	mov     rcx, 2
-	jmp     .7
 
-.3: mov     rax, [rsi]
-	mul     qword [rsi+8]
-	mov     [rdi+8], rax
-	mov     rax, [rsi]
-	mov     r8, rdx
-	mul     qword [rsi+16]
-	add     r8, rax
-	adc     rdx, 0
-	mov     [rdi+16], r8
-	mov     [rdi+24], rdx
-	mov     rax, [rsi+8]
-	mul     qword [rsi+16]
-	add     [rdi+24], rax
-	adc     rdx, 0
-	mov     [rdi+32], rdx
-	mov     rcx, 3
-	jmp     .7
-
-; this code can not handle casess 3,2,1
-.4:	mov     [rsp+stack_use+24], rdi     ; use shadow area
+    mov     [rsp+stack_use+24], rdi     ; use shadow area
 	mov     [rsp+stack_use+16], rsi
 	mov     [rsp+stack_use+ 8], rdx
 
 	mov     r13, [rsi]
-	mov     r14, 7
+	mov     r14d, 7
 	sub     r14, rdx
 	lea     rdi, [rdi+rdx*8-40]
 	lea     rsi, [rsi+rdx*8-40]
@@ -334,22 +367,21 @@ __gmpn_sqr_basecase:
 	lea     rdi, [rdi+8]
 	mov     rax, r14
 	and     rax, 3
-	je      .50
-	jp      .53
+	je      .60
+	jp      .63
 	cmp     rax, 1
-	je      .51
-
+	je      .61
 	align   16
-.5:
-.52:mpn_addmul_1_int 2
-.53:mpn_addmul_1_int 3
-.50:mpn_addmul_1_int 0
-.51:mpn_addmul_1_int 1
-	jmp     .5
+.6:
+.62:mpn_addmul_1_int 2
+.63:mpn_addmul_1_int 3
+.60:mpn_addmul_1_int 0
+.61:mpn_addmul_1_int 1
+	jmp     .6
 
-; only need to add one more line here
+; only need to add .1 more line here
 
-.6:	mov     rax, [rsi+r14*8]
+.7:	mov     rax, [rsi+r14*8]
 	mov     r13, [rsi+r14*8-8]
 	mul     r13
 	add     [rdi+r14*8], rax
@@ -362,7 +394,7 @@ __gmpn_sqr_basecase:
 	mov     rsi, [rsp + stack_use + 16]
 	mov     rdi, [rsp + stack_use + 24]
 
-.7: xor     rbx, rbx
+.8: xor     rbx, rbx
 	xor     r14, r14
 	lea     rsi, [rsi+rcx*8]
 	mov     [rdi], r14
@@ -370,7 +402,7 @@ __gmpn_sqr_basecase:
 	mov     [r10+rcx*8-8], r14
 	neg     rcx
 
-.8: mov     rax, [rsi+rcx*8]
+.9: mov     rax, [rsi+rcx*8]
 	mul     rax
 	mov     r8, [rdi]
 	mov     r9, [rdi+8]
@@ -386,8 +418,8 @@ __gmpn_sqr_basecase:
 	mov     [rdi+8], r9
 	inc     rcx
 	lea     rdi, [rdi+16]
-	jnz     .8
-	epilogue reg_save_list
+	jnz     .9
+
+.10:epilogue reg_save_list
 
 	end
-
