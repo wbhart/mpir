ac487c4e1c91bca57858afe8b504ba59f4552412
gladman <brg@gladman.plus.com>
2009-03-07 17:50:40 +0000

    1. Minor windows build update 2. Changes to try.c for Windows

--- tests/devel/try.c
+++ tests/devel/try.c
@@ -119,6 +119,11 @@ MA 02110-1301, USA. */
 #include <string.h>
 #include <time.h>
 
+#if defined( _MSC_VER )
+#define WINDOWS_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+
 #if HAVE_UNISTD_H
 #include <unistd.h>
 #endif
@@ -142,7 +147,7 @@ extern int optind, opterr;
 extern int sys_nerr;
 #endif
 
-#if ! HAVE_DECL_SYS_ERRLIST
+#if ! HAVE_DECL_SYS_ERRLIST && !defined( _MSC_VER )
 extern char *sys_errlist[];
 #endif
 
@@ -2855,17 +2860,19 @@ try_init (void)
   /* Prefer getpagesize() over sysconf(), since on SunOS 4 sysconf() doesn't
      know _SC_PAGESIZE. */
   pagesize = getpagesize ();
-#else
-#if HAVE_SYSCONF
+#elif HAVE_SYSCONF
   if ((pagesize = sysconf (_SC_PAGESIZE)) == -1)
     {
       /* According to the linux man page, sysconf doesn't set errno */
       fprintf (stderr, "Cannot get sysconf _SC_PAGESIZE\n");
       exit (1);
     }
+#elif defined( _MSC_VER )
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    return si.dwPageSize;
 #else
-Error, error, cannot get page size
-#endif
+#error Error, error, cannot get page size
 #endif
 
   printf ("pagesize is 0x%lX bytes\n", pagesize);
