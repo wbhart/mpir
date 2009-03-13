dc645759367dd3407c332ca0371ab44cd99e3bb7
gladman <brg@gladman.plus.com>
2009-03-13 10:22:20 +0000

    correct copyright notice spelling errors only in Windows assembler (caused by rsi -> rdx substitution)

--- mpn/x86_64w/core2/addsub_n.asm
+++ mpn/x86_64w/core2/addsub_n.asm
@@ -1,16 +1,16 @@
 
 ;  Core2 mpn_addsub_n
-;  Verdxon 1.0.4
+;  Version 1.0.4
 ;
 ;  Copyright 2008 Jason Moxham
 ;
-;  Windows Converdxon Copyright 2008 Brian Gladman
+;  Windows Conversion Copyright 2008 Brian Gladman
 ;
 ;  This file is part of the MPIR Library.
 ;  The MPIR Library is free software; you can redistribute it and/or modify
 ;  it under the terms of the GNU Lesser General Public License as published
-;  by the Free Software Foundation; either verdxon 2.1 of the License, or (at
-;  your option) any later verdxon.
+;  by the Free Software Foundation; either version 2.1 of the License, or (at
+;  your option) any later version.
 ;  The MPIR Library is distributed in the hope that it will be useful, but
 ;  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 ;  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
