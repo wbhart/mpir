00019039dec600d68feab056a7599f45493b1713
gladman <brg@gladman.plus.com>
2009-04-14 16:50:53 +0000

    Add missing files for tune on Windows

---
+++ build.vc9/tune/divrem_1.c
@@ -0,0 +1,3 @@
+#define TUNE_PROGRAM_BUILD 1
+#define __gmpn_divrem_1  mpn_divrem_1_tune
+#include "..\..\mpn\generic\divrem_1.c"
---
+++ build.vc9/tune/sb_divrem_mn.c
@@ -0,0 +1,2 @@
+#define TUNE_PROGRAM_BUILD 1
+#include "..\..\mpn\generic\sb_divrem_mn.c"
