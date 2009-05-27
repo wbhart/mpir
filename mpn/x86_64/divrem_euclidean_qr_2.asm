1ff359d28a4ac12bcffe2f6dbae105adf6ba9019
jasonmoxham <jason@njkfrudils.plus.com>
2009-05-27 11:21:58 +0000

    divrem_2 speedup

--- mpn/x86_64/divrem_euclidean_qr_2.asm
+++ mpn/x86_64/divrem_euclidean_qr_2.asm
@@ -25,114 +25,146 @@ C	ret mpn_test_ppn(mp_ptr,mp_ptr,mp_size_t)
 C	rax                 rdi,   rsi,      rdx
 
 PROLOGUE(mpn_divrem_euclidean_qr_2)
-#// t[0] is r8		t[1] is r9
-#//  l is   r10 		h is r11
-#//      divisor is rcx
-#// qn is rbx
-#// i is bp
-#// qf is r12
-#// mask,q is r13
+#	rax	MULTEMP
+#	rbx	qn
+#	rcx	dp
+#	rdx	MULTEMP
+#	rdi	qp
+#	rsi	xp
+#	rbp	i
+#	r8	l
+#	r9	h
+#	r10	t[0],xh
+#	r11	
+#	r12	t1[0]
+#	r13	t1[1]
+#	r14	qf
+#	r15	q,mask,temp
 push %rbx
 push %rbp
 push %r12
 push %r13
+push %r14
+push %r15
 mov %rdx,%rbx
-sub $3,%rbx
-xor %r11,%r11
-mov $-1,%rax
+dec %rbx
+mov $0,%r9
 mov 8(%rcx),%rdx
 not %rdx
+mov $-1,%rax
 divq 8(%rcx)
 mov %rax,%rbp
-mov 16(%rsi,%rbx,8),%r10
-mov 8(%rsi,%rbx,8),%r8
-cmp 8(%rcx),%r10
-jae j4
-   mov %r10,%r11
-   mov %r10,%r9
-   mov 8(%rsi,%rbx,8),%r10
+mov (%rsi,%rbx,8),%r8
+dec %rbx
+mov (%rsi,%rbx,8),%r10
+cmp 8(%rcx),%r8
+jae j2
+   mov %r8,%rdx
+   mov %r8,%r9
+   mov (%rsi,%rbx,8),%r10
    mov %r10,%r8
-   mov $0,%r12
-   jmp j5
-j4:
-   mov $1,%r12
-   mov %r10,%r9
-   sub 8(%rcx),%r9
-   sub (%rcx),%r8
-   sbb $0,%r9
+   mov $0,%r14
+   jmp j1
+j2:
+   mov $1,%r14
+   mov %r8,%rdx
+   sub 8(%rcx),%rdx
+   sub (%rcx),%r10
+   sbb $0,%rdx
    jnc j3
-      dec %r12
-      add (%rcx),%r8
-      adc 8(%rcx),%r9
+      dec %r14
+      add (%rcx),%r10
+      adc 8(%rcx),%rdx
    j3:
-   mov %r9,%r11
-   mov %r8,%r10
-j5:
-cmp $0,%rbx
+   mov %rdx,%r9
+   mov %r10,%r8
+j1:
+dec %rbx
+mov %r9,%rax
 js skiplp
-.align 16
-lp:
-    mov (%rsi,%rbx,8),%r8
-    cmp 8(%rcx),%r11
-    je j2
-       mov %r10,%r13
-       sar $63,%r13
-       mov %r11,%rax
-       sub %r13,%rax
-       and 8(%rcx),%r13
-       add %r10,%r13
-       mul %rbp
-       add %r13,%rax
-       adc %r11,%rdx
-       mov %rdx,%r13
-       not %rdx
-       mov 8(%rcx),%rax
-       mul %rdx
-       add %r10,%rax
-       adc %r11,%rdx
-       sub 8(%rcx),%rdx
-       sbb $-1,%r13
-       and 8(%rcx),%rdx
-       add %rax,%rdx
-       mov (%rcx),%rax
-       mov %rdx,%r9
-       mov %r13,(%rdi,%rbx,8)
-       mul %r13
-       sub %rax,%r8
-       sbb %rdx,%r9
-       jnc j1
-          decq (%rdi,%rbx,8)
-          add (%rcx),%r8
-          adc 8(%rcx),%r9
-          jc j1
-                decq (%rdi,%rbx,8)
-                add (%rcx),%r8
-                adc 8(%rcx),%r9
-                jmp j1
-       j2:
-       movq $-1,(%rdi,%rbx,8)
-       mov %r10,%r9
-       add (%rcx),%r8
-       adc %r11,%r9
-       sbb %rax,%rax
-       sub (%rcx),%r9
-       adc $0,%rax
-       jz j1
-          decq (%rdi,%rbx,8)
-          add (%rcx),%r8
-          adc %r11,%r9
-    j1:
-    mov %r9,%r11
-    mov %r8,%r10
+ALIGN(16)
+lp:    
+    cmp 8(%rcx),%rax
+    je j4
+        mov %rax,%r9
+        bt $63,%r8
+	adc $0,%rax
+	mov %r8,%r15
+	mov $0,%r13
+	sar $63,%r15
+	and 8(%rcx),%r15
+	add %r8,%r15
+	mul %rbp
+	add %r15,%rax
+	mov (%rcx),%rax
+	adc %r9,%rdx
+	mov %rdx,%r10
+	mul %rdx
+	mov (%rcx),%r12
+	add %rax,%r12
+	adc %rdx,%r13	
+	mov 8(%rcx),%rax
+	not %r10
+	mul %r10
+	sub 8(%rcx),%r9
+	add %r8,%rax
+	adc %r9,%rdx
+	mov %rdx,%r9
+	and 8(%rcx),%r9
+	# swapped r9,rax from here
+	add %r9,%rax
+	mov %rdx,%r15
+	sub %r10,%r15
+	and (%rcx),%rdx
+	sub %rdx,%r12
+	sbb $0,%r13
+	mov (%rsi,%rbx,8),%r8
+	sub %r12,%r8
+	sbb %r13,%rax
+	mov (%rcx),%r10
+	mov 8(%rcx),%r11
+	sbb %r9,%r9
+	and %r9,%r10
+	and %r9,%r11
+	add %r9,%r15
+	add %r10,%r8
+	adc %r11,%rax
+	adc $0,%r9
+	and %r9,%r10
+	and %r9,%r11
+	add %r9,%r15
+	add %r10,%r8
+	adc %r11,%rax
+    j6:
+    mov %r15,(%rdi,%rbx,8)
     dec %rbx
     jns lp
 skiplp:
-mov %r9,8(%rsi)
+mov %rax,8(%rsi)
 mov %r8,(%rsi)
-mov %r12,%rax
+mov %r14,%rax
+pop %r15
+pop %r14
 pop %r13
 pop %r12
 pop %rbp
 pop %rbx
 ret
+j4:
+       mov (%rsi,%rbx,8),%r10
+       mov $-1,%r15
+       mov %r8,%rdx
+       add (%rcx),%r10
+       adc %rax,%rdx
+       sbb %rax,%rax
+       sub (%rcx),%rdx
+       adc $0,%rax
+       jz j5
+ 	    dec %r15
+            add (%rcx),%r10
+	    adc 8(%rcx),%rdx	    
+       j5:
+       mov %rdx,%rax
+       mov %r10,%r8
+       jmp j6
 EPILOGUE()
