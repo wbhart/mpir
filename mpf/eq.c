7a33cd5980bbbfd6229431e37698aa1f1d6ffce7
jasonmoxham <jason@njkfrudils.plus.com>
2009-08-02 22:12:27 +0000

    fix trac 225 mpf_eq is broken

--- mpf/eq.c
+++ mpf/eq.c
@@ -1,32 +1,36 @@
 /* mpf_eq -- Compare two floats up to a specified bit #.
 
 Copyright 1993, 1995, 1996, 2001, 2002 Free Software Foundation, Inc.
+Copyright 2009 Jason Moxham
 
-This file is part of the GNU MP Library.
+This file is part of the MPIR Library.
 
-The GNU MP Library is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or (at your
-option) any later version.
+The MPIR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
 
-The GNU MP Library is distributed in the hope that it will be useful, but
+The MPIR Library is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-MA 02110-1301, USA. */
+along with the MPIR Library; see the file COPYING.LIB.  If not, write
+to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+*/
 
 #include "mpir.h"
 #include "gmp-impl.h"
+#include "longlong.h"
 
 int
 mpf_eq (mpf_srcptr u, mpf_srcptr v, unsigned long int n_bits)
 {
   mp_srcptr up, vp;
-  mp_size_t usize, vsize, size, i;
+  mp_size_t usize, vsize, size, i, cu, cv, n, k;
+  mp_limb_t uval, vval;
   mp_exp_t uexp, vexp;
 
   uexp = u->_mp_exp;
@@ -66,46 +70,35 @@ mpf_eq (mpf_srcptr u, mpf_srcptr v, unsigned long int n_bits)
   up = u->_mp_d;
   vp = v->_mp_d;
 
-  /* Ignore zeroes at the low end of U and V.  */
-  while (up[0] == 0)
+  count_leading_zeros (cu, up[usize - 1]);
+  count_leading_zeros (cv, vp[vsize - 1]);
+  if (cu != cv)
+    return 0;
+  n = BITS_TO_LIMBS (n_bits + cu);
+//compair bottom limb
+  k = n * GMP_NUMB_BITS - n_bits - cu;
+  uval = vval = 0;
+  if (usize - n >= 0)
+    uval = up[usize - n];
+  if (vsize - n >= 0)
+    vval = vp[vsize - n];
+  if ((uval >> k) != (vval >> k))
+    return 0;
+  if (n == 1)
+    return 1;
+//compair top limb
+  if (up[usize - 1] != vp[vsize - 1])
+    return 0;
+// compair middle limbs
+  for (i = usize - n + 1; i <= usize - 2; i++)
     {
-      up++;
-      usize--;
-    }
-  while (vp[0] == 0)
-    {
-      vp++;
-      vsize--;
-    }
-
-  if (usize > vsize)
-    {
-      if (vsize * GMP_NUMB_BITS < n_bits)
-	return 0;		/* surely too different */
-      size = vsize;
-    }
-  else if (vsize > usize)
-    {
-      if (usize * GMP_NUMB_BITS < n_bits)
-	return 0;		/* surely too different */
-      size = usize;
-    }
-  else
-    {
-      size = usize;
-    }
-
-  if (size > (n_bits + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS)
-    size = (n_bits + GMP_NUMB_BITS - 1) / GMP_NUMB_BITS;
-
-  up += usize - size;
-  vp += vsize - size;
-
-  for (i = size - 1; i >= 0; i--)
-    {
-      if (up[i] != vp[i])
+      uval = vval = 0;
+      if (i >= 0)
+	uval = up[i];
+      if (i - usize + vsize >= 0)
+	vval = vp[i - usize + vsize];
+      if (uval != vval)
 	return 0;
     }
-
   return 1;
 }
