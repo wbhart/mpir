a6147139228b8b3e1c3fc1813dbadf5643d86f09
jasonmoxham <jason@njkfrudils.plus.com>
2009-08-23 23:04:14 +0000

    New macro/function for AMD mpn_inclsh_n

---
+++ mpn/x86_64/k8/inclsh_n.c
@@ -0,0 +1,22 @@
+/*
+dnl  Copyright 2009 Jason Moxham
+
+dnl  This file is part of the MPIR Library.
+
+dnl  The MPIR Library is free software; you can redistribute it and/or modify
+dnl  it under the terms of the GNU Lesser General Public License as published
+dnl  by the Free Software Foundation; either version 2.1 of the License, or (at
+dnl  your option) any later version.
+
+dnl  The MPIR Library is distributed in the hope that it will be useful, but
+dnl  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+dnl  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+dnl  License for more details.
+
+dnl  You should have received a copy of the GNU Lesser General Public License
+dnl  along with the MPIR Library; see the file COPYING.LIB.  If not, write
+dnl  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+dnl  Boston, MA 02110-1301, USA.
+*/
+
+#define mpn_inclsh_n(__x,__y,__n,__c) mpn_addmul_1((__x),(__y),(__n),(((mp_limb_t)1)<<(__c)))
