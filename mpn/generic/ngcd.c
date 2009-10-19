ecdbd4a16272de97535781ee035d262a2bdba0c5
wbhart <goodwillhart@googlemail.com>
2009-10-19 05:49:29 +0000

    Added copyright info to GCD and XGCD code.

--- mpn/generic/ngcd.c
+++ mpn/generic/ngcd.c
@@ -1,15 +1,30 @@
-/* Schönhage's 1987 algorithm, reorganized into hgcd form */
+/* Schönhage's 1987 algorithm, reorganized into hgcd form 
+
+Copyright 2004, 2005 Niels Möller
+
+This file is part of the MPIR Library.
+
+The MPIR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+The MPIR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPIR Library; see the file COPYING.LIB.  If not, write
+to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
+*/
 
 #include <stdio.h>  /* for NULL */
 
 #include "mpir.h"
 #include "gmp-impl.h"
 #include "longlong.h"
-
-
-
-
-
 
 /* For input of size n, matrix elements are of size at most ceil(n/2)
    - 1, but we need one limb extra. */
--- mpn/generic/nhgcd2.c
+++ mpn/generic/nhgcd2.c
@@ -1,6 +1,25 @@
 /* nhgcd2
 
    hgcd2 with stop condition suitable for sgcd and ngcd
+
+Copyright 2004, 2005 Niels Möller
+
+This file is part of the MPIR Library.
+
+The MPIR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation; either version 2.1 of the License, or (at
+your option) any later version.
+
+The MPIR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPIR Library; see the file COPYING.LIB.  If not, write
+to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA 02110-1301, USA.
 */
 
 #include "mpir.h"
