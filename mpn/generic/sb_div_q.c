808c57b5b3708f020c12f41fd9d2d498a4c2d654
(no author) <(no author)>
2010-02-10 11:22:39 +0000

    Fixed some typos.

--- mpn/generic/sb_div_q.c
+++ mpn/generic/sb_div_q.c
@@ -25,7 +25,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 
 
-#include "gmp.h"
+#include "mpir.h"
 #include "gmp-impl.h"
 #include "longlong.h"
 
