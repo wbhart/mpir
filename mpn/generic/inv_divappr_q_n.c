b4e21c591f4ecb7d2361adabe43a9bc818bb249b
(no author) <(no author)>
2010-02-13 17:13:29 +0000

    Fixed approximate quotient code.

--- mpn/generic/inv_divappr_q_n.c
+++ mpn/generic/inv_divappr_q_n.c
@@ -47,22 +47,39 @@ mpn_inv_divappr_q_n(mp_ptr qp, mp_ptr np,
    tp = TMP_ALLOC_LIMBS(2*dn + 1);
    mpn_mul(tp, np + dn - 1, dn + 1, inv, dn);
    add_ssaaaa(cy, lo, 0, np[dn - 1], 0, tp[dn]);
-   mpn_add_n(qp, tp + dn + 1, np + dn, dn);
+   ret += mpn_add_n(qp, tp + dn + 1, np + dn, dn);
    ret += mpn_add_1(qp, qp, dn, cy + 1);
 
    /* 
       Let X = B^dn + inv, D = { dp, dn }, N = { np, 2*dn }, then
       DX < B^{2*dn} <= D(X+1), thus
-      NX < B^{2*dn}N/D <= NX + N < NX + B^{2*dn}, i.e.
-      NX/B^{2*dn} < N/D < NX/B^{2*dn} + 1, i.e.
-      NX//B^{2*dn} <= N//D <= NX//B^{2*dn} + 1.
+      Let N' = { np + n - 1, n + 1 }
+	  N'X/B^{dn+1} < B^{dn-1}N'/D <= N'X/B^{dn+1} + N'/B^{dn+1} < N'X/B^{dn+1} + 1
+      N'X/B^{dn+1} < N/D <= < N'X/B^{dn+1} + 1 + 2/B
+      There is either one integer in this range, or two. However, in the latter case
+	  the left hand bound is either an integer or < 2/B below one.
    */
-      
+     
    if (UNLIKELY(ret == 2))
    {
       ret = 1;
       mpn_sub_1(qp, qp, dn, 1);
    }
+   
+   if (UNLIKELY((lo == ~CNST_LIMB(0)) || (lo == ~CNST_LIMB(1)))) 
+   {
+	   /* Special case, multiply out to get accurate quotient */
+	   ret -= mpn_sub_1(qp, qp, dn, 1);
+	   mpn_mul_n(tp, qp, dp, dn);
+	   mpn_sub_n(tp, np, tp, dn + 1);
+       while (tp[dn] || mpn_cmp(tp, dp, dn) >= 0)
+	   {
+		   ret += mpn_add_1(qp, qp, dn, 1);
+		   tp[dn] -= mpn_sub_n(tp, tp, dp, dn);
+	   }
+       /* Not possible for ret == 2 as we have qp*dp <= np */
+   }
+
    TMP_FREE;
 
    return ret;
