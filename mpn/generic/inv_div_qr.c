dcc043e3ab1928e8091a0a312a361e785e750566
(no author) <(no author)>
2010-02-13 17:48:01 +0000

    Added code for balanced and unbalanced division with remainder using a precomputed inverse.

---
+++ mpn/generic/inv_div_qr.c
@@ -0,0 +1,201 @@
+/* mpn_inv_div_qr_n -- division with remainder for arbitrary
+   size operands using a precomputed inverse.
+
+   Contributed to the GNU project by Torbjorn Granlund.
+
+   THE FUNCTIONS IN THIS FILE ARE INTERNAL WITH MUTABLE INTERFACES.  IT IS ONLY
+   SAFE TO REACH THEM THROUGH DOCUMENTED INTERFACES.  IN FACT, IT IS ALMOST
+   GUARANTEED THAT THEY WILL CHANGE OR DISAPPEAR IN A FUTURE GMP RELEASE.
+
+Copyright 2006, 2007, 2009 Free Software Foundation, Inc.
+
+Copyright 2010 William Hart
+
+This file is part of the GNU MP Library.
+
+The GNU MP Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The GNU MP Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
+
+#include "mpir.h"
+#include "gmp-impl.h"
+#include "longlong.h"
+
+#define DC_DIV_QR_THRESHOLD 56 
+#define INV_DIV_QR_THRESHOLD 1200 
+
+mp_limb_t
+mpn_inv_div_qr (mp_ptr qp,
+		  mp_ptr np, mp_size_t nn,
+		  mp_srcptr dp, mp_size_t dn,
+		  mp_srcptr dinv)
+{
+  mp_size_t qn;
+  mp_limb_t qh, cy, dinv2;
+  mp_ptr tp;
+  TMP_DECL;
+
+  TMP_MARK;
+
+  ASSERT (dn >= 6);		/* to adhere to mpn_sbpi1_div_qr's limits */
+  ASSERT (nn - dn >= 3);	/* to adhere to mpn_sbpi1_div_qr's limits */
+  ASSERT (dp[dn-1] & GMP_NUMB_HIGHBIT);
+
+  invert_1(dinv2, dp[dn - 1], dp[dn - 2]);
+
+  tp = TMP_ALLOC_LIMBS (10 * dn);
+
+  qn = nn - dn;
+  qp += qn;
+  np += nn;
+  dp += dn;
+
+  if (qn > dn)
+    {
+      /* Reduce qn mod dn without division, optimizing small operations.  */
+      do
+	qn -= dn;
+      while (qn > dn);
+
+      qp -= qn;			/* point at low limb of next quotient block */
+      np -= qn;			/* point in the middle of partial remainder */
+
+      /* Perform the typically smaller block first.  */
+      if (qn == 1)
+	{
+	  mp_limb_t q, n2, n1, n0, d1, d0;
+
+	  /* Handle qh up front, for simplicity. */
+	  qh = mpn_cmp (np - dn + 1, dp - dn, dn) >= 0;
+	  if (qh)
+	    ASSERT_NOCARRY (mpn_sub_n (np - dn + 1, np - dn + 1, dp - dn, dn));
+
+	  /* A single iteration of schoolbook: One 3/2 division,
+	     followed by the bignum update and adjustment. */
+	  n2 = np[0];
+	  n1 = np[-1];
+	  n0 = np[-2];
+	  d1 = dp[-1];
+	  d0 = dp[-2];
+
+	  ASSERT (n2 < d1 || (n2 == d1 && n1 <= d0));
+
+	  if (UNLIKELY (n2 == d1) && n1 == d0)
+	    {
+	      q = GMP_NUMB_MASK;
+	      cy = mpn_submul_1 (np - dn, dp - dn, dn, q);
+	      ASSERT (cy == n2);
+	    }
+	  else
+	    {
+	      tdiv_qr_3by2 (q, n1, n0, n2, n1, n0, d1, d0, dinv2);
+
+	      if (dn > 2)
+		{
+		  mp_limb_t cy, cy1;
+		  cy = mpn_submul_1 (np - dn, dp - dn, dn - 2, q);
+
+		  cy1 = n0 < cy;
+		  n0 = (n0 - cy) & GMP_NUMB_MASK;
+		  cy = n1 < cy1;
+		  n1 = (n1 - cy1) & GMP_NUMB_MASK;
+		  np[-2] = n0;
+
+		  if (UNLIKELY (cy != 0))
+		    {
+		      n1 += d1 + mpn_add_n (np - dn, np - dn, dp - dn, dn - 1);
+		      qh -= (q == 0);
+		      q = (q - 1) & GMP_NUMB_MASK;
+		    }
+		}
+	      else
+		np[-2] = n0;
+
+	      np[-1] = n1;
+	    }
+	  qp[0] = q;
+	}
+      else
+	{
+      /* Do a 2qn / qn division */
+	  if (qn == 2)
+	    qh = mpn_divrem_2 (qp, 0L, np - 2, 4, dp - 2); /* FIXME: obsolete function. Use 5/3 division? */
+	  else if (BELOW_THRESHOLD (qn, DC_DIV_QR_THRESHOLD))
+	    qh = mpn_sb_div_qr (qp, np - qn, 2 * qn, dp - qn, qn, dinv2);
+	  else if (BELOW_THRESHOLD (qn, INV_DIV_QR_THRESHOLD))
+	    qh = mpn_dc_div_qr_n (qp, np - qn, dp - qn, qn, dinv2, tp);
+	  else
+	    qh = mpn_inv_div_qr_n (qp, np - qn, dp - qn, qn, dinv);
+
+	  if (qn != dn)
+	    {
+	      if (qn > dn - qn)
+		mpn_mul (tp, qp, qn, dp - dn, dn - qn);
+	      else
+		mpn_mul (tp, dp - dn, dn - qn, qp, qn);
+
+	      cy = mpn_sub_n (np - dn, np - dn, tp, dn);
+	      if (qh != 0)
+		cy += mpn_sub_n (np - dn + qn, np - dn + qn, dp - dn, dn - qn);
+
+	      while (cy != 0)
+		{
+		  qh -= mpn_sub_1 (qp, qp, qn, 1);
+		  cy -= mpn_add_n (np - dn, np - dn, dp - dn, dn);
+		}
+	    }
+	}
+
+      qn = nn - dn - qn;
+      do
+	{
+	  qp -= dn;
+	  np -= dn;
+	  mpn_inv_div_qr_n (qp, np - dn, dp - dn, dn, dinv, tp);
+	  qn -= dn;
+	}
+      while (qn > 0);
+    }
+  else
+    {
+      qp -= qn;			/* point at low limb of next quotient block */
+      np -= qn;			/* point in the middle of partial remainder */
+
+      if (BELOW_THRESHOLD (qn, DC_DIV_QR_THRESHOLD))
+	qh = mpn_sb_div_qr (qp, np - qn, 2 * qn, dp - qn, qn, dinv2);
+      else if (BELOW_THRESHOLD (qn, INV_DIV_QR_THRESHOLD))
+	qh = mpn_dc_div_qr_n (qp, np - qn, dp - qn, qn, dinv2, tp);
+	  else
+	qh = mpn_inv_div_qr_n (qp, np - qn, dp - qn, qn, dinv);
+
+      if (qn != dn)
+	{
+	  if (qn > dn - qn)
+	    mpn_mul (tp, qp, qn, dp - dn, dn - qn);
+	  else
+	    mpn_mul (tp, dp - dn, dn - qn, qp, qn);
+
+	  cy = mpn_sub_n (np - dn, np - dn, tp, dn);
+	  if (qh != 0)
+	    cy += mpn_sub_n (np - dn + qn, np - dn + qn, dp - dn, dn - qn);
+
+	  while (cy != 0)
+	    {
+	      qh -= mpn_sub_1 (qp, qp, qn, 1);
+	      cy -= mpn_add_n (np - dn, np - dn, dp - dn, dn);
+	    }
+	}
+    }
+
+  TMP_FREE;
+  return qh;
+}
---
+++ mpn/generic/inv_div_qr_n.c
@@ -0,0 +1,85 @@
+/* inv_div_qr_n - quotient and remainder using a precomputed inverse 
+
+Copyright 2010 William Hart
+
+This file is part of the MPIR Library.
+
+The MPIR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPIR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPIR Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#include <mpir.h>
+#include "gmp-impl.h"
+#include "longlong.h"
+
+/* 
+   Computes the quotient and remainder of { np, 2*dn } by { dp, dn }.
+   We require dp to be normalised and inv to be a precomputed inverse 
+   of { dp, dn } given by mpn_invert.
+*/
+mp_limb_t 
+mpn_inv_div_qr_n(mp_ptr qp, mp_ptr np, 
+                           mp_srcptr dp, mp_size_t dn, mp_srcptr inv)
+{
+   mp_limb_t cy, lo, ret = 0;
+   mp_ptr tp;
+   TMP_DECL;
+
+   TMP_MARK;
+
+   if (mpn_cmp(np + dn, dp, dn) >= 0)
+   {
+      ret = 1;
+      mpn_sub_n(np + dn, np + dn, dp, dn);
+   }
+
+   tp = TMP_ALLOC_LIMBS(2*dn + 1);
+   mpn_mul(tp, np + dn - 1, dn + 1, inv, dn);
+   add_ssaaaa(cy, lo, 0, np[dn - 1], 0, tp[dn]);
+   ret += mpn_add_n(qp, tp + dn + 1, np + dn, dn);
+   ret += mpn_add_1(qp, qp, dn, cy);
+
+   /* 
+      Let X = B^dn + inv, D = { dp, dn }, N = { np, 2*dn }, then
+      DX < B^{2*dn} <= D(X+1), thus
+      Let N' = { np + n - 1, n + 1 }
+	  N'X/B^{dn+1} < B^{dn-1}N'/D <= N'X/B^{dn+1} + N'/B^{dn+1} < N'X/B^{dn+1} + 1
+      N'X/B^{dn+1} < N/D <= < N'X/B^{dn+1} + 1 + 2/B
+      There is either one integer in this range, or two. However, in the latter case
+	  the left hand bound is either an integer or < 2/B below one.
+   */
+     
+   if (UNLIKELY(ret == 2))
+   {
+      ret = 1;
+      mpn_sub_1(qp, qp, dn, 1);
+   }
+   
+   /* Special case, multiply out to get accurate quotient */
+   ret -= mpn_sub_1(qp, qp, dn, 1); /* ret is now guaranteed to be 0 */
+   mpn_mul_n(tp, qp, dp, dn);
+   mpn_sub_n(np, np, tp, 2*dn);
+   while (np[dn] || mpn_cmp(np, dp, dn) >= 0)
+   {
+	   ret += mpn_add_1(qp, qp, dn, 1);
+	   np[dn] -= mpn_sub_n(np, np, dp, dn);
+   }
+   
+   /* Not possible for ret == 2 as we have qp*dp <= np */
+
+   TMP_FREE;
+
+   return ret;
+}
+
