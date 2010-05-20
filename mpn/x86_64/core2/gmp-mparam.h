e153633e9ecce8f048b37449dcc44fab16fc3e80
mvngu <nguyenminh2@gmail.com>
2010-05-20 04:03:05 +0000

    core2: tuning values for GCD_THRESHOLD and GCDEXT_THRESHOLD, averaged out for Linux and OS X

--- mpn/x86_64/core2/gmp-mparam.h
+++ mpn/x86_64/core2/gmp-mparam.h
@@ -14,7 +14,8 @@
 #define POWM_THRESHOLD                   62
 
 #define GCD_ACCEL_THRESHOLD              50
-#define GCDEXT_THRESHOLD                151
+#define GCD_THRESHOLD                   635
+#define GCDEXT_THRESHOLD                871
 #define JACOBI_BASE_METHOD                1
 
 #define DIVREM_1_NORM_THRESHOLD       MP_SIZE_T_MAX  /* never */
