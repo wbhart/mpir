24733c963ee5140758d763196c3194c583be22a1
gladman <brg@gladman.plus.com>
2010-06-27 10:22:11 +0000

    Update the Python gas to yasm translator for Visual Studio 2010

--- build.vc10/g2y.py
+++ build.vc10/g2y.py
@@ -117,18 +117,18 @@ r_mrf = re.compile(r'([A-Z$_][A-Z0-9$_]*)\s*\(' + r1 + r2 +
                   r2 + r2 + r2 + r2 + r2 + r2 + r2 + r2 + r'\)')
 
 def lab_ref(v, labels, macros, mac_name):
-  if mac_name and v in macros[mac_name][3] :
+  if mac_name and v in macros[mac_name][3]:
     return '%%{0}'.format(macros[mac_name][3][v])
-  elif dot_labels :
+  elif dot_labels:
     return '.{0}'.format(labels.index(v))
-  else :
+  else:
     return 'L_{0}'.format(re.sub('\$', '%', v))
 
-def pass_one(code) :
+def pass_one(code):
   labels = []
-  for l in code :
+  for l in code:
     m = re.match(r'(\s*)(?:C|#|;|dnl)\s*(.*)', l)
-    if m :
+    if m:
       v = list(m.groups())
       continue
     m = m_la.match(l)
@@ -138,16 +138,16 @@ def pass_one(code) :
         labels += [lb]
   return labels
 
-def pass_two(code, labels) :
+def pass_two(code, labels):
 
   mac_name = ''
   mac_dict = {}
   lab_idx = 0
-  for ln in range(len(code)) :
+  for ln in range(len(code)):
     l = code[ln]
 
     m = r_mac.search(l)
-    if m and not mac_name :
+    if m and not mac_name:
       mac_name = m.group(1)
       mac_dict[mac_name] = [ln, 0, 0, {}]
       continue
@@ -159,23 +159,23 @@ def pass_two(code, labels) :
       lab_idx = 0
       continue
 
-    if mac_name :
+    if mac_name:
       m = r_dlr.findall(l)
-      if m :
+      if m:
         mn = max(int(i) for i in m)
-        if mn > mac_dict[mac_name][2] :
+        if mn > mac_dict[mac_name][2]:
           mac_dict[mac_name][2] = mn
 
       m = m_la.match(l)
       if m: 
         lb = m.group(1) if m.group(1) else m.group(2)      
-        if lb in labels :
+        if lb in labels:
           lab_idx += 1
           mac_dict[mac_name][3][lb] = lab_idx
 
   return mac_dict
 
-def addr(dd, labels, macros, mac_name) :
+def addr(dd, labels, macros, mac_name):
   pfx = '['
   ss = ']'
   if dd['lab1'] or dd['lab2']:
@@ -196,14 +196,14 @@ def addr(dd, labels, macros, mac_name) :
     ss = dd['reg1'] + ss
   return pfx + ss
 
-def pass_three(code, labels, macros, level) :
+def pass_three(code, labels, macros, level):
 
   lo = []
   mac_name = ''
-  for l in code :
+  for l in code:
 
     m = re.match(r'(\s*)(?:C|#|;|dnl)\s*(.*)', l)
-    if m :
+    if m:
       v = list(m.groups())
       if not remove_comment_lines:
         lo += ['{0[0]}; {0[1]}'.format(v)]
@@ -211,7 +211,7 @@ def pass_three(code, labels, macros, level) :
 
     m = re.search(r'(.*)\s+#(.*)$', l)
     sfx = ''
-    if m :
+    if m:
       if m.group(1):
         l = m.group(1) + ' '
       else:
@@ -225,29 +225,29 @@ def pass_three(code, labels, macros, level) :
     if m:
       lb = m.group(1) if m.group(1) else m.group(2)      
       l = l[ l.find(':') + 1 : -1 ]
-      if mac_name :
-        if lb in macros[mac_name][3] :
+      if mac_name:
+        if lb in macros[mac_name][3]:
           ii = macros[mac_name][3][lb]
-        else :
+        else:
           print('internal error')
-      else :
+      else:
         ii = labels.index(lb)
       if lb:
         lb = re.sub('\$', '%', lb)
-        if not l :
-          if mac_name :
+        if not l:
+          if mac_name:
             lo += ['\n%%{0}:'.format(ii)]
-          elif dot_labels :
+          elif dot_labels:
             lo += ['\n.{0}:'.format(ii)]
-          else :
+          else:
             lo += ['\nL_{0}:'.format(lb)]
             continue
-        else :
-          if mac_name :
+        else:
+          if mac_name:
             lp = '\n%%{0}:'.format(ii)
-          elif dot_labels :
+          elif dot_labels:
             lp = '\n.{0}:'.format(ii)
-          else :
+          else:
             lp = '\nL_{0}:'.format(lb)
     
     if not re.search(r'(\S+)', l):
@@ -259,9 +259,9 @@ def pass_three(code, labels, macros, level) :
           
     # three operand instructions
     m = m_fa.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       e = d['imm'] if d['imm'] else d['reg1']
       lo += [lp + '\t{0[ins]:7s} {0[reg3]}, {0[reg2]}, {1}'.format(d, e)]
@@ -269,8 +269,8 @@ def pass_three(code, labels, macros, level) :
 
     # ins reg, dis(reg, reg, off)
     m = m_f1.search(l)
-    if m :
-      if debug :
+    if m:
+      if debug:
         print(l, end = '')
       d = m.groupdict()
       s = addr(d, labels, macros, mac_name)
@@ -279,8 +279,8 @@ def pass_three(code, labels, macros, level) :
 
     # ins dis(reg, reg, off), reg
     m = m_f2.search(l)
-    if m :
-      if debug :
+    if m:
+      if debug:
         print(l, end = '')
       d = m.groupdict()
       s = addr(d, labels, macros, mac_name)
@@ -289,8 +289,8 @@ def pass_three(code, labels, macros, level) :
 
     # ins dis(reg, reg, off)
     m = m_f3.search(l)
-    if m :
-      if debug :
+    if m:
+      if debug:
         print(l, end = '')
       d = m.groupdict()      
       s = addr(d, labels, macros, mac_name)
@@ -299,8 +299,8 @@ def pass_three(code, labels, macros, level) :
 
     # ins imm, dis(reg, reg, off)
     m = m_f4.search(l)
-    if m :
-      if debug :
+    if m:
+      if debug:
         print(l, end = '')
       d = m.groupdict()
       s = addr(d, labels, macros, mac_name)
@@ -309,57 +309,57 @@ def pass_three(code, labels, macros, level) :
 
     # ins imm, reg
     m = m_f5.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       lo += [lp + '\t{0[ins]:7s} {0[reg1]}, {0[imm]}'.format(d)]
       continue
 
     # ins reg, reg
     m = m_f6.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       lo += [lp + '\t{0[ins]:7s} {0[reg2]}, {0[reg1]}'.format(d)]
       continue
 
     # ins reg
     m = m_f7.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       lo += [lp + '\t{0[ins]:7s} {0[reg1]}'.format(d)]
       continue
 
     # ins *reg
     m = m_g7.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       lo += [lp + '\t{0[ins]:7s} {0[reg1]}'.format(d)]
       continue
 
     # ins imm
     m = m_f8.search(l)
-    if m :
+    if m:
       d = m.groupdict()
-      if debug :
+      if debug:
         print(l, end = '')
       lo += [lp + '\t{0[ins]:7s} {0[imm]}'.format(d)]
       continue
 
     # jump table
     m = m_jt.search(l)
-    if m :
+    if m:
       d = m.groupdict()
       lb0 = d['lab1'] if d['lab1'] else d['lab2']
       lb1 = d['lab3'] if d['lab3'] else d['lab4']
-      if lb0 in labels and lb1 in labels :
-        if debug :
+      if lb0 in labels and lb1 in labels:
+        if debug:
           print(l, end = '')
         st1 = lab_ref(lb0, labels, macros, mac_name)
         st2 = lab_ref(lb1, labels, macros, mac_name)
@@ -368,142 +368,142 @@ def pass_three(code, labels, macros, level) :
 
     # jump label
     m = m_f9.search(l)
-    if m :
+    if m:
       d = m.groupdict()
       lb = d['lab1'] if d['lab1'] else d['lab2']
-      if lb and lb in labels :
-        if debug :
+      if lb and lb in labels:
+        if debug:
           print(l, end = '')
         ss = lab_ref(lb, labels, macros, mac_name)
         lo += [lp + '\t{0[ins]:7s} {1}'.format(d, ss)]
         continue
 
     m = re.search(r'\s*\.byte\s+((?:0x|0X)[0-9a-fA-F]+|[0-9]+)\s*', l)
-    if m :
+    if m:
       v = list(m.groups())
       lo += [lp + '\tdb      {0[0]}'.format(v)]
       continue
 
     # macro definitions
     m = r_mac.search(l)
-    if m :
+    if m:
       mac_name = m.group(1)
-      if mac_name in macros :
+      if mac_name in macros:
         lo += [lp + '%macro ' + mac_name.lower() + ' '
                         + str(macros[mac_name][2])]
         continue
 
     m = r_mnd.search(l)
-    if m and mac_name :
+    if m and mac_name:
       mac_name = ''
       lab_ofs = 0
       lo += [lp + '%endmacro']
       continue
 
     m = r_dlr.findall(l)
-    if m :
+    if m:
       l = re.sub(r'\$([0-9]+)', r'%\1', l)
 
     m = re.search('PROLOGUE\(([a-zA-Z$_][a-zA-Z0-9$_]*)\)', l)
-    if m :
-      if is_linux :
+    if m:
+      if is_linux:
         lo += [lp + '\tGLOBAL_FUNC {0}'.format(m.group(1))]
-      else :
+      else:
         lo += [lp + '\tWIN64_GCC_PROC {0}'.format(m.group(1))]
       continue
 
     m = re.search('EPILOGUE\(\)', l)
-    if m :
-      if is_linux :
+    if m:
+      if is_linux:
         lo += [lp + '\tend']
-      else :
+      else:
         lo += [lp + '\tWIN64_GCC_END']
       continue
 
     # macro calls
     m = r_mrz.search(l)
-    if not m :
+    if not m:
       m = r_mrf.search(l)
-    if m :
-      if m.group(1).lower() == 'align' :
+    if m:
+      if m.group(1).lower() == 'align':
         fs = '\talign   {0}' if is_linux else '\txalign  {0}'
         lo += [lp + fs.format(m.group(2))]
         continue
-      elif m.group(1) in macros :
+      elif m.group(1) in macros:
         lp += '\t{0:7s}'.format(m.group(1).lower())
         i = 2
-        while i <= m.lastindex and m.group(i) :
+        while i <= m.lastindex and m.group(i):
           lp += ' {0}'.format(m.group(i))
           i += 1
         lo += [lp]
         continue
-      elif m.group(1) and (m.lastindex == 1 or not m.group(2)) :
+      elif m.group(1) and (m.lastindex == 1 or not m.group(2)):
         lo += [lp + '\t{0}'.format(m.group(1))]
         continue
 
-    if mac_name :
+    if mac_name:
       m = re.search(r'\s*([^%]+)%([0-9]+)\s*', l)
-      if m and m.lastindex == 2 and int(m.group(2)) <= macros[mac_name][2] :
+      if m and m.lastindex == 2 and int(m.group(2)) <= macros[mac_name][2]:
         lo += [lp + '\t{0}%{1}'.format(m.group(1).lower(),m.group(2))]
         continue
 
     # ins
     m = re.search(p_in + r'\s+(.*)', l)
-    if m :
+    if m:
       v = list(m.groups())
-      if debug :
+      if debug:
         print(l, end = '')
-      if len(v) == 1 or len(v) == 2 and v[1] == '' :
-        if v[0] in macros :
+      if len(v) == 1 or len(v) == 2 and v[1] == '':
+        if v[0] in macros:
           lo += [lp + '\t{0}'.format(v[0].lower())]
-        else :
+        else:
           lo += [lp + '\t{0[0]}'.format(v)]
         continue
-      elif v[0] == 'C' :
+      elif v[0] == 'C':
         lo += [lp + ';\t{0}'.format(v[1])]
         continue
 
     m = re.search(r'include\(.+config.m4.+\)', l)
-    if m :
-      if is_linux :
+    if m:
+      if is_linux:
         lo += [lp + "%include 'yasm_mac.inc'"]
-      else :
+      else:
         lo += [lp + "%include '"
                + ''.join(['..\\'] * level) + "yasm_mac.inc'"]
       continue
 
     m = re.search(r'\s*(ret\b.*)', l)
-    if m :
+    if m:
       lo += [lp + '{0}'.format(l.rstrip(string.whitespace))]
       continue
 
     m = re.search(r'\s*(\S+)', l)
-    if m :
-      if len(l) :
+    if m:
+      if len(l):
         lo += [lp + '{0} ; < not translated >'
                .format(l.rstrip(string.whitespace))]
-      else :
+      else:
         lo += [lp]
     elif lp:
       lo += [lp]
 
   return lo + ['\n']
 
-def form_path(p) :
+def form_path(p):
   n = str.rfind(p, '\\', 0, -1)
-  if n != -1 :
-    if not os.path.exists(p[ : n + 1]) :
+  if n != -1:
+    if not os.path.exists(p[ : n + 1]):
       form_path(p[ : n + 1])
-  if p[-1] == '\\' and not os.path.exists(p) :
+  if p[-1] == '\\' and not os.path.exists(p):
     os.mkdir(p)
 
-def conv_lines(code, l) :
+def conv_lines(code, l):
   labels = pass_one(code)
   macros = pass_two(code, labels)
   code = pass_three(code, labels, macros, l)
   return (labels, macros, code)
 
-def conv_file(f_in, f_out, l) :
+def conv_file(f_in, f_out, l):
   f = open(f_in, 'r')
   code = f.readlines()
   f.close()
@@ -512,64 +512,64 @@ def conv_file(f_in, f_out, l) :
   f.writelines(code)
   f.close()
 
-def conv_dirs(s, d, l) :
+def conv_dirs(s, d, l):
   fd = os.listdir(s)
-  for f in fd :
+  for f in fd:
     sp = os.path.join(s,f)
     dp = os.path.join(d,f)
-    if os.path.isdir(sp) :
+    if os.path.isdir(sp):
       conv_dirs(sp, dp, l + 1)
-    elif os.path.isfile(sp) :
-      if sp != dp and os.path.exists(dp) :
+    elif os.path.isfile(sp):
+      if sp != dp and os.path.exists(dp):
         continue
       fn = os.path.basename(f)
       x = os.path.splitext(fn)
-      if x[1] == '.asm' :
+      if x[1] == '.asm':
         form_path(dp)
         print("translating '{0}'".format(sp))
         f = open(sp, 'r')
         code = f.readlines()
         f.close()
-        if sp == dp :
+        if sp == dp:
           rp = os.path.join(s, x[0] + ('.as' if is_linux else '.old'))
           os.rename(sp, rp)
         labels, macros, code = conv_lines(code, l)
         f = open(dp, 'w')
         f.writelines(code)
         f.close()
-      elif False :
+      elif False:
         form_path(dp)
         shutil.copyfile(sp, dp)
 
-if len(sys.argv) == 1 :
+if len(sys.argv) == 1:
   cd = os.getcwd()                    # if run in the build.vc9 directory  
-  if cd.endswith('build.vc9') :
+  if cd.endswith('build.vc10'):
     cd1 = cd + '\\..\\mpn\\x86_64'    # the GAS assembler directory
     cd2 = cd + '\\..\\mpn\\x86_64w'   # the YASM (Windows) assembler directory
-  elif cd.endswith('x86_64') :        # if run in the GAS assembler directory
+  elif cd.endswith('x86_64'):         # if run in the GAS assembler directory
     cd1 = cd
     cd2 = cd + '\\..\\x86_64w'
-  elif cd.endswith('x86_64w') :       # if run in the YASM assembler directory
+  elif cd.endswith('x86_64w'):        # if run in the YASM assembler directory
     cd2 = cd
     cd1 = cd + '\\..\\x86_64'
-  else :
+  else:
     print("cannot locate assembler source directory")
     sys.exit(-1)
-elif len(sys.argv) == 3 :
+elif len(sys.argv) == 3:
   cd1 = sys.argv[1]
   cd2 = sys.argv[2]
-else :
+else:
   print("invalid program location or input")
   sys.exit(-1)
 
-if cd1 and os.path.exists(cd1) :
-  if os.path.isdir(cd1) :
-    if os.path.exists(cd2) and os.path.isdir(cd2) :
+if cd1 and os.path.exists(cd1):
+  if os.path.isdir(cd1):
+    if os.path.exists(cd2) and os.path.isdir(cd2):
       print("warning: output directory '{0}' already exists".format(cd2))
-    if cd1 != cd2 :
+    if cd1 != cd2:
       conv_dirs(cd1, cd2, 0)          # convert format from GAS to YASM
-  elif os.path.isfile(cd1) :
-    if not os.path.exists(cd2) :
+  elif os.path.isfile(cd1):
+    if not os.path.exists(cd2):
       conv_file(cd1, cd2, 0)
-else :
+else:
   print('{0} is not a file or directory'.format(cd1))
