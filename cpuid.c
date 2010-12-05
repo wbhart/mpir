73b48a44d045ca7f8f24cd95be00588b8f1562df
jasonmoxham <jason@njkfrudils.plus.com>
2010-12-05 11:10:26 +0000

    add model 44 westmere hexa-core

--- cpuid.c
+++ cpuid.c
@@ -86,6 +86,7 @@ Boston, MA 02110-1301, USA.
 	  if (model == 30){ CPUIS(westmere);break;}
 	  if (model == 37){ CPUIS(westmere);break;}
 	  if (model == 42){ CPUIS(sandybridge);break;}
+	  if (model == 44){ CPUIS(westmere);break;}
           break;
         case 15:
         #if CONFIG_GUESS_64BIT || FAT64
