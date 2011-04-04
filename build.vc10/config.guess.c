bc8f665803f580cc66bd33e98e8a81236ca57660
jasonmoxham <jason@njkfrudils.plus.com>
2011-04-04 00:43:00 +0000

    and for windows

--- build.vc10/config.guess.c
+++ build.vc10/config.guess.c
@@ -28,9 +28,6 @@ memcpy(p+4,&(a[3]),4);
 memcpy(p+8,&(a[2]),4);
 return a[0];}
 
-main (int argc,char *argv[])
-{char *p;int a=sizeof(p)*8;
-
 #define CONFIG_GUESS		1
 // safe to always try 32bit
 #define CONFIG_GUESS_32BIT	1
@@ -39,5 +36,10 @@ main (int argc,char *argv[])
 #define FAT64			0
 #define INFAT			0
 #include "../cpuid.c"
+
+main (int argc,char *argv[])
+{char *p,*modelstr;int a=sizeof(p)*8;
+
+modelstr=__gmpn_cpu(0);
 if(argc==2){printf("set GCPU=%s\nset GBITS=%d\n",modelstr,a);return 0;}
 printf("%s-pc-Win%d\n",modelstr,a);return 0;}
