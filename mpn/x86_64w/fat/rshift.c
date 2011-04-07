3ea0bbfc8be1500e119d9096fd753d3e7b9f6bc8
jasonmoxham <jason@njkfrudils.plus.com>
2011-04-07 14:21:29 +0000

    and for x64w

---
+++ mpn/x86_64w/fat/rshift.c
@@ -0,0 +1,23 @@
+/* Fat binary fallback mpn_rshift
+
+Copyright 2011 Jason Moxham
+
+This file is part of the MPIR Library.
+
+The MPIR Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPIR Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+
+#include "mpn/generic/rshift.c"
