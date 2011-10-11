5ff8f72cbf98bdf9e21b8da2bc6c52442403b8ea
gladman <brg@gladman.plus.com>
2011-10-11 21:59:03 +0000

    bring into full sync with my current code

--- mpn/generic/gcdext_1.c
+++ mpn/generic/gcdext_1.c
@@ -22,10 +22,8 @@ along with the GNU MP Library.  If not, see http://www.gnu.org/licenses/.  */
 #include "gmp-impl.h"
 #include "longlong.h"
 
-#if 1
-
 #ifndef GCDEXT_1_USE_BINARY
-#define GCDEXT_1_USE_BINARY 1
+#define GCDEXT_1_USE_BINARY 0
 #endif
 
 #ifndef GCDEXT_1_BINARY_METHOD
@@ -318,94 +316,3 @@ mpn_gcdext_1 (mp_limb_signed_t *up, mp_limb_signed_t *vp,
     }
 }
 #endif /* !GCDEXT_1_USE_BINARY */
-
-#else
-
-mp_limb_t mpn_gcdext_1(mp_limb_signed_t * a, mp_limb_signed_t *b, mp_limb_t x, mp_limb_t y)
-{
-   mp_limb_signed_t u1 = CNST_LIMB(1); 
-   mp_limb_signed_t u2 = CNST_LIMB(0); 
-   mp_limb_signed_t v1 = CNST_LIMB(0); 
-   mp_limb_signed_t v2 = CNST_LIMB(1); 
-   mp_limb_signed_t t1, t2; 
-   mp_limb_t u3, v3;
-   mp_limb_t quot, rem;
-   
-   u3 = x, v3 = y;
-   
-   if (v3 > u3)
-   {
-         rem = u3;
-         t1 = u2; u2 = u1; u1 = t1; u3 = v3;
-         t2 = v2; v2 = v1; v1 = t2; v3 = rem;
-   }
-   
-   if ((mp_limb_signed_t) (x & y) < (mp_limb_signed_t) CNST_LIMB(0)) // x and y both have top bit set  
-   {
-     quot=u3-v3;
-     t2 = v2; 
-     t1 = u2; u2 = u1 - u2; u1 = t1; u3 = v3;
-     v2 = v1 - v2; v1 = t2; v3 = quot;
-   }
-
-   while ((mp_limb_signed_t) (v3<<1) < (mp_limb_signed_t) CNST_LIMB(0)) // second value has second msb set 
-   {
-     quot=u3-v3;
-     if (quot < v3)
-     {
-        t2 = v2; 
-        t1 = u2; u2 = u1 - u2; u1 = t1; u3 = v3;
-        v2 = v1 - v2; v1 = t2; v3 = quot;
-     } else if (quot < (v3<<1))
-     {  
-        t1 = u2; u2 = u1 - (u2<<1); u1 = t1; u3 = v3;
-        t2 = v2; v2 = v1 - (v2<<1); v1 = t2; v3 = quot-u3;
-     } else
-     {
-        t1 = u2; u2 = u1 - 3*u2; u1 = t1; u3 = v3;
-        t2 = v2; v2 = v1 - 3*v2; v1 = t2; v3 = quot-(u3<<1);
-     }
-   }
-   
-   while (v3) {
-      quot=u3-v3;
-      if (u3 < (v3<<2)) // overflow not possible due to top 2 bits of v3 not being set 
-      {
-         if (quot < v3)
-         {
-            t2 = v2; 
-            t1 = u2; u2 = u1 - u2; u1 = t1; u3 = v3;
-            v2 = v1 - v2; v1 = t2; v3 = quot;
-         } else if (quot < (v3<<1))
-         {  
-            t1 = u2; u2 = u1 - (u2<<1); u1 = t1; u3 = v3;
-            t2 = v2; v2 = v1 - (v2<<1); v1 = t2; v3 = quot-u3;
-         } else
-         {
-            t1 = u2; u2 = u1 - 3*u2; u1 = t1; u3 = v3;
-            t2 = v2; v2 = v1 - 3*v2; v1 = t2; v3 = quot-(u3<<1);
-         }
-      } else
-      {
-         quot=u3/v3;
-         rem = u3 - v3*quot;
-         t1 = u2; u2 = u1 - quot*u2; u1 = t1; u3 = v3;
-         t2 = v2; v2 = v1 - quot*v2; v1 = t2; v3 = rem;
-      }
-   }
-   
-   // Quite remarkably, this always has |u1| < x/2 at this point, thus comparison with 0 is valid 
-   /*if (u1 <= (mp_limb_signed_t) 0) 
-   {    
-	 u1 += y;
-     v1 -= x;
-   } */
-
-   *a = u1;
-   *b = -v1;
-   
-   return u3;
- 
-}
-
-#endif
