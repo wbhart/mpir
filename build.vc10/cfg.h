954ccc087c4b201a9a5818694f7f1ac0855d1741
gladman <brg@gladman.plus.com>
2012-03-12 12:58:54 +0000

    Turn off stdint.h if Visual Studio earlier than 2010

--- build.vc10/cfg.h
+++ build.vc10/cfg.h
@@ -20,7 +20,7 @@ along with the GNU MP Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
-#ifndef HAVE_STDINT_H 
+#if _MSC_VER >= 1600 && !defined( HAVE_STDINT_H ) 
 #  define HAVE_STDINT_H 1
 #endif
 
