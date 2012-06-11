f43cb59137241b7349bd490d73f1023ba5dde322
gladman <brg@gladman.plus.com>
2012-06-11 20:31:25 +0000

    further improvements to Windows build generator allow generic mpn_sumdiff_n to work with zero limbs

--- mpn/generic/sumdiff_n.c
+++ mpn/generic/sumdiff_n.c
@@ -33,6 +33,8 @@ ASSERT(MPN_SAME_OR_SEPARATE_P(d,x,n));
 ASSERT(MPN_SAME_OR_SEPARATE_P(d,y,n));
 ASSERT(!MPN_OVERLAP_P(s,d,n));
 
+if(n == 0)
+	return 0;
 if( (s==x && d==y)||(s==y && d==x) )
   {t=__GMP_ALLOCATE_FUNC_LIMBS(n);
    ret=mpn_sub_n(t,x,y,n);
