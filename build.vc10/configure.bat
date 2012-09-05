5205d30ddab9d46b324a4ae7162e3b5d4e33908b
gladman <brg@gladman.plus.com>
2012-09-05 14:11:27 +0000

    Update to batch file build for MSVC provided by Case Remove macro bug found by Case

--- build.vc10/configure.bat
+++ build.vc10/configure.bat
@@ -1,5 +1,5 @@
 @echo off
-::  Copyright 2009 Jason Moxham
+::  Copyright 2009,2011 Jason Moxham
 ::
 ::  This file is part of the MPIR Library.
 ::
@@ -17,11 +17,16 @@
 ::  along with the MPIR Library; see the file COPYING.LIB.  If not, write
 ::  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ::  Boston, MA 02110-1301, USA.
+
+
+:: set default lib type as static
 set LIBTYPE=lib
+:: set default abi to ?
+set ABI=?
+:: set default cpu to ?
 set CPU=?
-set BITS=?
-set GBITS=?
-set GCPU=?
+
+:: parse params
 :lp
 shift
 if "%0" == "" goto :exitlp
@@ -29,116 +34,161 @@ if "%0" == "--enable-shared" ( set LIBTYPE=dll)
 if "%0" == "--enable-static" ( set LIBTYPE=lib)
 if "%0" == "--disable-shared" ( set LIBTYPE=lib)
 if "%0" == "--disable-static" ( set LIBTYPE=dll)
-if "%0" == "--ABI32" ( set BITS=32)
-if "%0" == "--ABI64" ( set BITS=64)
-if "%0" == "--cpu-none" ( set CPU=none)
-if "%0" == "--cpu-x86_64" ( set CPU=x86_64)
-if "%0" == "--cpu-i486" ( set CPU=i486)
-if "%0" == "--cpu-pentium" ( set CPU=pentium)
-if "%0" == "--cpu-pentiummmx" ( set CPU=pentiummmx)
-if "%0" == "--cpu-pentiumpro" ( set CPU=pentiumpro)
-if "%0" == "--cpu-pentium2" ( set CPU=pentium2)
-if "%0" == "--cpu-pentium3" ( set CPU=pentium3)
-if "%0" == "--cpu-core" ( set CPU=core)
-if "%0" == "--cpu-core2" ( set CPU=core2)
-if "%0" == "--cpu-penryn" ( set CPU=penryn)
-if "%0" == "--cpu-nehalem" ( set CPU=nehalem)
-if "%0" == "--cpu-atom" ( set CPU=atom)
-if "%0" == "--cpu-netburst" ( set CPU=netburst)
-if "%0" == "--cpu-netburstlahf" ( set CPU=netburstlahf)
-if "%0" == "--cpu-pentium4" ( set CPU=pentium4)
-if "%0" == "--cpu-prescott" ( set CPU=prescott)
-if "%0" == "--cpu-k5" ( set CPU=k5)
-if "%0" == "--cpu-k6" ( set CPU=k6)
-if "%0" == "--cpu-k62" ( set CPU=k62)
-if "%0" == "--cpu-k63" ( set CPU=k63)
-if "%0" == "--cpu-k7" ( set CPU=k7)
-if "%0" == "--cpu-k8" ( set CPU=k8)
-if "%0" == "--cpu-k10" ( set CPU=k10)
-if "%0" == "--cpu-viac3" ( set CPU=viac3)
-if "%0" == "--cpu-viac32" ( set CPU=viac32)
+if "%0" == "ABI" (
+	shift
+	set ABI=%1
+)
+if "%0" == "--cpu" (
+	shift
+	set CPU=%1
+)
 goto :lp
 :exitlp
-if %BITS% == ? ( 
-	set BITS=64
-	if %PROCESSOR_ARCHITECTURE% == x86 (set BITS=32)
+
+:: ARCH is native ABI
+set ARCH=64
+if %PROCESSOR_ARCHITECTURE% == x86 ( set ARCH=32)
+set VCTARGET=
+if %ARCH% == 64 (
+	if %ABI% == 64 (set VCTARGET=amd64)
+	if %ABI% == 32 (set VCTARGET=x86)
 )
-set ARCH=x86
-set ARCHW=Win32
-if %BITS% == 64 (
-	set ARCH=amd64
-	set ARCHW=x64
+if %ARCH% == 32 (
+	if %ABI% == 64 (set VCTARGET=amd64_x86)
+	if %ABI% == 32 (set VCTARGET=x86)
 )
-cl config.guess.c > nul 2>&1
-if errorlevel 1 goto :nocc1
+
+:: now we have to find a compiler that matches the required abi or default=?
+copy ..\build.vc10\config.guess.c . > nul 2>&1
+cl /nologo config.guess.c > nul 2>&1
+if errorlevel 1 goto :step1
 config.guess.exe print > config.guess.bat
 call config.guess.bat
-if %GBITS% == %BITS% goto :gotcc
-:nocc1
+if %ABI% == ? goto :gotcc
+if %GBITS% == %ABI% goto :gotcc
+:step1
 if exist "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" (
-	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %ARCH%
-        set YASM="%VS100COMNTOOLS%\..\..\VC\bin\vsyasm.exe"
+	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %VCTARGET%
+	goto :checkcc
 )
-cl config.guess.c > nul 2>&1
+if exist "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" (
+	call "%VS90COMNTOOLS%\..\..\VC\vcvarsall.bat" %VCTARGET%
+	goto :checkcc
+)
+
+:nocc
+echo cant find a suitible compiler
+exit /b 1
+
+:checkcc
+cl /nologo config.guess.c > nul 2>&1
 if errorlevel 1 goto :nocc
 config.guess.exe print > config.guess.bat
 call config.guess.bat
-if %GBITS% == %BITS% goto :gotcc
-:nocc
-echo Cant find a compiler for %ARCH%
-exit /b 1
+if %ABI% == ? goto :gotcc
+if %GBITS% == %ABI% goto :gotcc
+goto :nocc
 :gotcc
+if %ABI% == ? ( set ABI=%GBITS%)
+if %CPU% == ? ( set CPU=%GCPU%)
 
-if not exist %YASM% (
-	echo Cant find %YASM%
-	echo Download from http://www.tortall.net/projects/yasm/ and install to above
-	exit /b 1
+:: now find yasm , not needed for C build?
+if %CPU% == none goto :gotyasm
+set YASMEXE=yasm.exe
+yasm.exe --version > nul 2>&1
+if not errorlevel 1 goto :gotyasm
+set YASMEXE=vsyasm.exe
+vsyasm.exe --version > nul 2>&1
+if not errorlevel 1 goto :gotyasm
+if exist "%YASMPATH%\yasm.exe" (
+	set YASMEXE="%YASMPATH%\yasm.exe"
+	goto :gotyasm
 )
+if exist "%YASMPATH%\vsyasm.exe" (
+	set YASMEXE="%YASMPATH%\vsyasm.exe"
+	goto :gotyasm
+)
+if exist "%VS100COMNTOOLS%\..\..\VC\bin\vsyasm.exe" (
+	set YASMEXE="%VS100COMNTOOLS%\..\..\VC\bin\vsyasm.exe"
+	goto :gotyasm
+)
+if exist "c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\BuildCustomizations\vsyasm.exe" (
+	set YASMEXE="c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\BuildCustomizations\vsyasm.exe"
+	goto :gotyasm
+)
+if exist "%VS90COMNTOOLS%\..\..\VC\bin\yasm.exe" (
+	set YASMEXE="%VS90COMNTOOLS%\..\..\VC\bin\yasm.exe"
+	goto :gotyasm
+)
+echo cant find yasm
+exit /b 1
+:gotyasm
 
-if %CPU% == ? (	set CPU=%GCPU%)
-set BCPU=none
-if %BITS% == 64 (
-	if %CPU% == x86_64 ( set BCPU=gc)
-	if %CPU% == core2 ( set BCPU=core2)
-	if %CPU% == penryn ( set BCPU=core2)
-	if %CPU% == nehalem ( set BCPU=nehalem)
-	if %CPU% == atom ( set BCPU=gc)
-	if %CPU% == netburst ( set BCPU=gc)
-	if %CPU% == netburstlahf ( set BCPU=gc)
-	if %CPU% == k8 ( set BCPU=k8)
-	if %CPU% == k10 ( set BCPU=k10)
+:: set config.params.bat to the settings needed by make etc
+echo (set LIBTYPE=%LIBTYPE%) > config.params.bat
+echo (set FLAGS=/Ox /Ot /D "NDEBUG" /D "HAVE_CONFIG_H" /nologo /D "_MBCS" /GS-) >> config.params.bat
+if %LIBTYPE% == lib (set FLAGS1=/Oi /D "_LIB" /D "PIC" /MT)
+if %LIBTYPE% == dll (set FLAGS1=/D "__GMP_LIBGMP_DLL" /D "_WINDLL" /GF /EHsc /MD)
+echo (set FLAGS1=%FLAGS1%) >> config.params.bat
+set MPNPATH=.
+if %ABI% == 32 (
+	if %CPU% == x86		(set MPNPATH=x86w)
+	if %CPU% == i386	(set MPNPATH=x86w)
+	if %CPU% == i486	(set MPNPATH=x86w)
+	if %CPU% == i586	(set MPNPATH=x86w)
+	if %CPU% == pentium	(set MPNPATH=x86w)
+	if %CPU% == pentiummmx	(set MPNPATH=x86w)
+	if %CPU% == pentiumpro	(set MPNPATH=x86w x86w\p6)
+	if %CPU% == i686	(set MPNPATH=x86w x86w\p6)
+	if %CPU% == pentium2	(set MPNPATH=x86w x86w\p6 x86w\p6\mmx)
+	if %CPU% == pentium3	(set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == k6		(set MPNPATH=x86w)
+	if %CPU% == k62		(set MPNPATH=x86w)
+	if %CPU% == k63		(set MPNPATH=x86w)
+	if %CPU% == k7		(set MPNPATH=x86w)
+	if %CPU% == athlon	(set MPNPATH=x86w)
+	if %CPU% == pentium4	(set MPNPATH=x86w x86w\pentium4 x86w\pentium4\mmx x86w\pentium4\sse2)
+	if %CPU% == prescott	(set MPNPATH=x86w x86w\pentium4 x86w\pentium4\mmx x86w\pentium4\sse2)
+	if %CPU% == core	(set MPNPATH=x86w x86w\pentium4 x86w\pentium4\mmx x86w\pentium4\sse2)
+	if %CPU% == viac3	(set MPNPATH=x86w)
+	if %CPU% == viac32	(set MPNPATH=x86w)
+	if %CPU% == x86_64	(set MPNPATH=x86w)
+	if %CPU% == netburst	(set MPNPATH=x86w x86w\pentium4 x86w\pentium4\mmx x86w\pentium4\sse2)
+	if %CPU% == netburstlahf	(set MPNPATH=x86w x86w\pentium4 x86w\pentium4\mmx x86w\pentium4\sse2)
+	if %CPU% == k8           (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == k10          (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == k102         (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == k103         (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == bulldozer    (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == bobcat       (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == core2        (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == penryn       (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == nehalem      (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == westmere     (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == sandybridge  (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == atom         (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
+	if %CPU% == nano         (set MPNPATH=x86w x86w\p6 x86w\p6\mmx x86w\p6\p3mmx)
 )
-if %BITS% == 32 (
-	if %CPU% == i486 ( set BCPU=gc)
-	if %CPU% == pentium ( set BCPU=p0)
-	if %CPU% == pentiummmx ( set BCPU=p0)
-	if %CPU% == pentiumpro ( set BCPU=p0)
-	if %CPU% == pentium2 ( set BCPU=p0)
-	if %CPU% == pentium3 ( set BCPU=p3)
-	if %CPU% == core ( set BCPU=p3)
-	if %CPU% == core2 ( set BCPU=p3)
-	if %CPU% == penryn ( set BCPU=p3)
-	if %CPU% == nehalem ( set BCPU=p3)
-	if %CPU% == atom ( set BCPU=p3)
-	if %CPU% == netburst ( set BCPU=p4)
-	if %CPU% == netburstlahf ( set BCPU=p4)
-	if %CPU% == pentium4 ( set BCPU=p4)
-	if %CPU% == prescott ( set BCPU=p4)
-	if %CPU% == k5 ( set BCPU=p0)
-	if %CPU% == k6 ( set BCPU=p0)
-	if %CPU% == k62 ( set BCPU=p0)
-	if %CPU% == k63 ( set BCPU=p0)
-	if %CPU% == k7 ( set BCPU=p3)
-	if %CPU% == k8 ( set BCPU=p3)
-	if %CPU% == k10 ( set BCPU=p3)
-	if %CPU% == viac3 ( set BCPU=p0)
-	if %CPU% == viac32 ( set BCPU=p0)
+if %ABI% == 64 (
+	if %CPU% == x86_64       (set MPNPATH=x86_64w)
+	if %CPU% == netburst     (set MPNPATH=x86_64w x86_64w\netburst)
+	if %CPU% == netburstlahf (set MPNPATH=x86_64w x86_64w\netburst)
+	if %CPU% == k8           (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k8only)
+	if %CPU% == k10          (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k10)
+	if %CPU% == k102         (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k10 x86_64w\k8\k10\k102)
+	if %CPU% == k103         (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k10 x86_64w\k8\k10\k102)
+	if %CPU% == bulldozer    (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k10 x86_64w\k8\k10\k102)
+	if %CPU% == bobcat       (set MPNPATH=x86_64w x86_64w\bobcat)
+	if %CPU% == core2        (set MPNPATH=x86_64w x86_64w\core2)
+	if %CPU% == penryn       (set MPNPATH=x86_64w x86_64w\core2 x86_64w\core2\penryn)
+	if %CPU% == nehalem      (set MPNPATH=x86_64w x86_64w\nehalem)
+	if %CPU% == westmere     (set MPNPATH=x86_64w x86_64w\nehalem x86_64w\nehalem\westmere)
+	if %CPU% == sandybridge  (set MPNPATH=x86_64w x86_64w\sandybridge)
+	if %CPU% == atom         (set MPNPATH=x86_64w x86_64w\atom)
+	if %CPU% == nano         (set MPNPATH=x86_64w x86_64w\k8 x86_64w\k8\k8only)
 )
-if %CPU% == none ( set BCPU=gc)
-echo Detected %GCPU%
-echo (set LIBTYPE=%LIBTYPE%) > config_params.bat
-echo (set ARCHW=%ARCHW%) >> config_params.bat
-echo (set BCPU=%BCPU%) >> config_params.bat
+echo (set MPNPATH=%MPNPATH%) >> config.params.bat
+echo (set ABI=%ABI%) >> config.params.bat
 echo setting params to
-type config_params.bat
+type config.params.bat
 exit /b 0
--- build.vc10/make.bat
+++ build.vc10/make.bat
@@ -1,5 +1,5 @@
 @echo off
-::  Copyright 2009 Jason Moxham
+::  Copyright 2011 Jason Moxham
 ::
 ::  This file is part of the MPIR Library.
 ::
@@ -18,98 +18,414 @@
 ::  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ::  Boston, MA 02110-1301, USA.
 ::
-if not exist config_params.bat (
+
+if not exist config.params.bat (
 	echo run configure first
 	exit /b 1
 )
-call config_params.bat
-if "%1" == ""        goto :make
-if "%1" == "clean"   goto :clean
-if "%1" == "install" goto :install
-if "%1" == "check"   goto :check
-if "%1" == "speed"   goto :speed
-if "%1" == "tune"    goto :tune
-if "%1" == "try"     goto :try
-if "%1" == "help" (
-	echo Usage : make [clean^|install^|check^|speed^|tune^|try^|help]
-	exit /b 0
-)
-echo Unkwown option
+call config.params.bat
+if "%1" == "" goto :make
+if "%1" == "check" goto :check
+if "%1" == "clean" goto :clean
+if "%1" == "tune" goto :tune
+if "%1" == "speed" goto :speed
+echo Usage: make [clean^|check^|tune^|speed]
 exit /b 1
 
 :make
 
-set MS_BUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+md mpn fft mpz mpq mpf printf scanf cxx > nul 2>&1
 
-set LIBBUILD=%LIBTYPE%_mpir_%BCPU%
+copy ..\build.vc10\gen_mpir_h.bat .
+copy ..\build.vc10\out_copy_rename.bat .
+copy ..\build.vc10\gen_config_h.bat .
+copy ..\build.vc10\cfg.h .
 
-if errorlevel 1 (
-	echo "ERROR PREBUILD"
-	exit /b 1
+if %ABI% == 64 (set LOCALABI=x64)
+if %ABI% == 32 (set LOCALABI=win32)
+set YASMFLAG=
+if %LIBTYPE% == dll (set YASMFLAG=-D DLL)
+if %LIBTYPE% == dll (set FLAGS1=%FLAGS1% /D "__GMP_WITHIN_GMP" /D "__GMP_WITHIN_GMPXX")
+call gen_mpir_h %LOCALABI%
+copy ..\mpn\generic\gmp-mparam.h .. > nul 2>&1
+copy ..\longlong_pre.h+..\mpn\generic\longlong_inc.h+..\longlong_post.h ..\longlong.h > nul 2>&1
+for %%X in ( %MPNPATH% ) do (
+	copy ..\mpn\%%X\gmp-mparam.h .. > nul 2>&1
+	copy ..\longlong_pre.h+..\mpn\%%X\longlong_inc.h+..\longlong_post.h ..\longlong.h > nul 2>&1
+	call gen_config_h ..\mpn\%%X\ > nul 2>&1
 )
-%MS_BUILD% %LIBBUILD%\%LIBBUILD%.vcxproj /p:Platform=%ARCHW%;Configuration=Release
 
+set HAVE_STDINT=yes
+echo #include ^<stdint.h^> > comptest.c
+echo int main(void){return 0;} >> comptest.c
+cl /nologo comptest.c > nul 2>&1
 if errorlevel 1 (
-	echo "ERROR BUILDING"
-	exit /b 1
+	echo #undef HAVE_STDINT_H >> ..\config.h
+	echo #undef HAVE_INTMAX_T >> ..\config.h
+	echo #undef HAVE_UINTMAX_T >> ..\config.h
+	echo #undef HAVE_PTRDIFF_T >> ..\config.h
+	echo #undef HAVE_UINT_LEAST32_T >> ..\config.h
+	echo #undef SIZEOF_UINTMAX_T >> ..\config.h
+	set HAVE_STDINT=no
 )
-:: c++ to build  if static
-if %LIBTYPE% == lib (
-	%MS_BUILD% lib_mpir_cxx\lib_mpir_cxx.vcxproj /p:Platform=%ARCHW%;Configuration=Release
-	if errorlevel 1 (
-		echo "ERROR BUILDING CXX"
-		exit /b 1
+del comptest.*
+
+if %ABI% == 64 (echo #define HAVE_NATIVE_mpn_addmul_2 1 >> ..\mpir.h)
+
+if %ABI% == 64 (set OPT=/D "_WIN64" %FLAGS% %FLAGS1% /c)
+if %ABI% == 32 (set OPT=%FLAGS% %FLAGS1% /c)
+
+if %ABI% == 64 (set LOCALDIR=x86_64w)
+if %ABI% == 32 (set LOCALDIR=x86w)
+:: just compile all generic and just overwrite with asm
+cd mpn
+for %%X in ( ..\..\mpn\generic\*.c) do (
+	:: exclude udiv_w_sdiv.c from all builds
+	if not "%%X" == "..\..\mpn\generic\udiv_w_sdiv.c" (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
 	)
 )
+for %%X in ( %MPNPATH% ) do (
+	for %%i in ( ..\..\mpn\%%X\*.asm ) do (
+		%YASMEXE% %YASMFLAG% -I ..\..\mpn\%LOCALDIR% -f %LOCALABI% %%i
+		if errorlevel 1 goto :err
+		echo assemblin %%i		
+	)
+)
+:: dont know what the asm version have so delete them
+del preinv_divrem_1.obj preinv_mod_1.obj divrem_1.obj mod_1.obj divrem_euclidean_qr_1.obj > nul 2>&1
+cl %OPT% -I..\.. ..\..\mpn\generic\divrem_1.c
+if errorlevel 1 goto :err
+cl %OPT% -I..\.. ..\..\mpn\generic\mod_1.c
+if errorlevel 1 goto :err
+cl %OPT% -I..\.. ..\..\mpn\generic\divrem_euclidean_qr_1.c
+if errorlevel 1 goto :err
+cl /D "USE_PREINV_DIVREM_1" %OPT% -I..\.. ..\..\mpn\generic\preinv_divrem_1.c
+if errorlevel 1 goto :err
+cl %OPT% -I..\.. ..\..\mpn\generic\preinv_mod_1.c
+if errorlevel 1 goto :err
+cd ..
+
+cd mpz
+for %%X in ( ..\..\mpz\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+if %HAVE_STDINT% == no (
+	del *_sx.obj *_ux.obj > nul 2>&1
+)
+cd ..
+
+cd fft
+for %%X in ( ..\..\fft\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd mpf
+for %%X in ( ..\..\mpf\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd mpq
+for %%X in ( ..\..\mpq\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd printf
+for %%X in ( ..\..\printf\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd scanf
+for %%X in ( ..\..\scanf\*.c) do (
+	cl %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+for %%X in ( ..\assert.c ..\errno.c ..\extract-dbl.c ..\invalid.c ..\memory.c ..\mp_bpl.c ..\mp_clz_tab.c ..\mp_dv_tab.c ..\mp_get_fns.c ..\mp_minv_tab.c ..\mp_set_fns.c ..\randbui.c ..\randclr.c ..\randdef.c ..\randiset.c ..\randlc2s.c ..\randlc2x.c ..\randmt.c ..\randmts.c ..\randmui.c ..\rands.c ..\randsd.c ..\randsdui.c ..\tal-reent.c ..\version.c ) do (
+	cl %OPT% -I.. %%X
+	if errorlevel 1 goto :err
+)
+
+cd cxx
+for %%X in ( ..\..\cxx\*.cc) do (
+	cl /EHsc %OPT% -I..\.. %%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+if %LIBTYPE% == dll (
+	link /DLL /NODEFAULTLIB:LIBCMT.lib /nologo scanf\*.obj printf\*.obj mpz\*.obj fft\*.obj mpq\*.obj mpf\*.obj mpn\*.obj *.obj /out:mpir.%LIBTYPE%
+	if errorlevel 1 goto :err
+	link /DLL /NODEFAULTLIB:LIBCMT.lib /nologo scanf\*.obj printf\*.obj mpz\*.obj fft\*.obj mpq\*.obj mpf\*.obj mpn\*.obj *.obj /out:mpirxx.%LIBTYPE%
+	if errorlevel 1 goto :err
+)
+if %LIBTYPE% == lib (
+	lib /nologo scanf\*.obj printf\*.obj mpz\*.obj fft\*.obj mpq\*.obj mpf\*.obj mpn\*.obj *.obj /out:mpir.%LIBTYPE%
+	if errorlevel 1 goto :err
+	lib /nologo cxx/*.obj /out:mpirxx.%LIBTYPE%
+	if errorlevel 1 goto :err
+)
 exit /b 0
+
+
+
+
+
+
+
 
 :check
-:: this gives an error if we dont build the c++ stuff
-%MS_BUILD% mpir-tests.sln  /p:Platform=%ARCHW%;Configuration=Release"
-if errorlevel 1 (
-	echo "ERROR BUILDING TESTS"
-	exit /b 1
+
+md tests
+cd tests
+md mpn fft mpz mpq mpf rand misc cxx devel > nul 2>&1
+if %LIBTYPE% == dll (
+	for %%X in ( mpn fft mpz mpq mpf rand misc cxx devel .) do (
+		copy ..\mpir.dll %%X
+	)
+	copy ..\mpirxx.dll cxx
 )
-cd mpir-tests
-@echo off
-for /f "usebackq tokens=1*" %%f in (`reg query HKCR\Python.File\shell\open\command`) do (set _my_=%%f %%g)
-if not errorlevel 1 goto :got
-echo ERROR Could not find PYTHON
-exit /b 1
-:got
-set _res_=%_my_:*REG_SZ=%
-set _end_=%_res_:*exe"=%
-call set _python_=%%_res_:%_end_%=%%
-call %_python_% run-tests.py noenter
-set RET=%ERRORLEVEL%
 cd ..
-exit /b %RET%
 
-:install
-echo HOW???
+set OPT=%FLAGS% %FLAGS1%
+
+set MPIRLIB=/link ..\..\mpir.lib
+if %LIBTYPE% == lib (set MPIRLIB=..\..\mpir.lib)
+if %LIBTYPE% == lib (set MPIRXXLIB=..\..\mpirxx.lib)
+
+set MPIRLIB1=/link ..\mpir.lib
+if %LIBTYPE% == lib (set MPIRLIB1=..\mpir.lib)
+
+cd tests
+
+cl %OPT% /c ..\..\tests\memory.c /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\misc.c   /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\trace.c  /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\refmpn.c /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\refmpz.c /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\refmpq.c /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\tests\refmpf.c /I..\..
+if errorlevel 1 goto :err
+:: these only needed for try.exe
+cl %OPT% /c ..\..\tests\spinner.c /I..\..
+if errorlevel 1 goto :err
+cl %OPT% /c ..\..\build.vc10\getopt.c /I..\..
+if errorlevel 1 goto :err
+
+for %%X in ( ..\..\tests\t-*.c) do (
+	cl %OPT% /I..\.. /I..\..\tests %%X misc.obj memory.obj trace.obj refmpn.obj %MPIRLIB1% 
+	if errorlevel 1 goto :err	
+)
+for %%X in ( *.exe) do (
+	echo testing %%X
+	%%X
+	if errorlevel 1 goto :err
+)
+
+cd mpn
+for %%X in ( ..\..\..\tests\mpn\t-*.c) do (
+	if not %%X == ..\..\..\tests\mpn\t-fat.c (
+		cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj %MPIRLIB%
+		if errorlevel 1 goto :err
+	)
+)
+for %%X in ( *.exe) do (
+	echo testing mpn_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd fft
+for %%X in ( ..\..\..\tests\fft\t-*.c) do (
+	if not %%X == ..\..\..\tests\fft\t-fat.c (
+		cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj %MPIRLIB%
+		if errorlevel 1 goto :err
+	)
+)
+for %%X in ( *.exe) do (
+	echo testing fft_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd mpz
+for %%X in ( ..\..\..\tests\mpz\*.c) do (
+	cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj ..\refmpz.obj %MPIRLIB%
+	if errorlevel 1 goto :err
+)
+for %%X in ( *.exe) do (
+	echo testing mpz_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd mpq
+for %%X in ( ..\..\..\tests\mpq\t-*.c) do (
+	cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj ..\refmpz.obj ..\refmpq.obj %MPIRLIB%
+	if errorlevel 1 goto :err
+)
+for %%X in ( *.exe) do (
+	echo testing mpq_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd mpf
+for %%X in ( ..\..\..\tests\mpf\*.c) do (
+	cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj ..\refmpf.obj %MPIRLIB%
+	if errorlevel 1 goto :err
+)
+for %%X in ( *.exe) do (
+	echo testing mpf_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd rand
+for %%X in ( ..\..\..\tests\rand\t-*.c) do (
+	cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj %MPIRLIB%
+	if errorlevel 1 goto :err
+)
+for %%X in ( *.exe) do (
+	echo testing rand_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd misc
+for %%X in ( ..\..\..\tests\misc\t-*.c) do (
+	if not "%%X" == "..\..\..\tests\misc\t-locale.c" (
+	    cl %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj %MPIRLIB%
+	    if errorlevel 1 goto :err
+	)
+)
+for %%X in ( *.exe) do (
+	echo testing misc_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd cxx
+for %%X in ( ..\..\..\tests\cxx\t-*.cc) do (
+	cl /EHsc %OPT% /I..\..\.. /I..\..\..\tests %%X ..\misc.obj ..\memory.obj ..\trace.obj ..\refmpn.obj %MPIRLIB% %MPIRXXLIB%
+	if errorlevel 1 goto :err
+)
+for %%X in ( *.exe) do (
+	echo testing cxx_%%X
+	%%X
+	if errorlevel 1 goto :err
+)
+cd ..
+
+cd devel
+cl %OPT% ..\..\..\tests\devel\try.c /I..\..\..\ /I..\..\..\tests ..\refmpn.obj ..\refmpz.obj ..\trace.obj ..\spinner.obj ..\misc.obj ..\memory.obj ..\getopt.obj %MPIRLIB%
+if errorlevel 1 goto :err
+cd ..
+
+cd ..
+
+exit /b 0
+
+
+:err
+echo ERROR
 exit /b 1
 
-:speed
-%MS_BUILD% speed\speed.vcxproj /p:Platform=%ARCHW%;Configuration=Release
-echo speed.exe is in %ARCHW%\Release\
-exit /b 0
 
-:tune
-%MS_BUILD% tune\tune.vcxproj /p:Platform=%ARCHW%;Configuration=Release
-echo tune.exe is in %ARCHW%\Release\
-exit /b 0
 
-:try
-%MS_BUILD% try\try.vcxproj /p:Platform=%ARCHW%;Configuration=Release
-echo try.exe is in try\%ARCHW%\Release\
-exit /b 0
+
 
 :clean
-del config_params.bat config.guess.bat config.guess.exe config.guess.obj last_build.txt >nul 2>&1
-del ..\config.h ..\mpir.h ..\gmp.h ..\gmp-mparam.h >nul 2>&1
-del gen-psqr\gen-psqr.exe gen-fac_ui\gen-fac_ui.exe gen-bases\gen-bases.exe >nul 2>&1
-del gen-fib\gen-fib.exe gen-mpir\gen-mpir.exe gen-mpir\gen-mpir.pdb >nul 2>&1
-rmdir /s/q x64 win32 %LIBTYPE%_mpir_%BCPU%\%ARCHW% lib_mpir_cxx\%ARCHW% lib\%ARCHW% dll\%ARCHW% mpir-tests\%ARCHW% >nul 2>&1
-rmdir /s/q gen-psqr\Win32 gen-fac_ui\Win32 gen-bases\Win32 gen-fib\Win32 gen-mpir\Win32 >nul 2>&1
-rmdir /s/q lib_speed\%ARCHW% speed\%ARCHW% tune\%ARCHW% try\%ARCHW% >nul 2>&1
+
+del *.obj ..\mpir.h ..\config.h ..\gmp-mparam.h mpir.lib *.idb *.pdb > nul 2>&1
+rmdir /S/Q mpn fft mpz mpq mpf scanf printf tests cxx tune speed > nul 2>&1
+del gen_mpir_h.bat out_copy_rename.bat gen_config_h.bat cfg.h > nul 2>&1
+del getopt.h getrusage.h gettimeofday.h unistd.h win_timing.h > nul 2>&1 
+del config.guess.* config.params.bat mpir.dll mpir.dll.manifest mpir.exp > nul 2>&1
+del mpirxx.lib mpirxx.dll mpirxx.exp mpirxx.dll.manifest > nul 2>&1
 exit /b 0
+
+
+
+
+:speed
+
+md speed
+
+set OPT=%FLAGS% %FLAGS1%
+set MPIRLIB=
+if %LIBTYPE% == lib (set MPIRLIB=mpir.lib)
+
+copy ..\build.vc10\unistd.h .
+copy ..\build.vc10\getopt.h .
+copy ..\build.vc10\win_timing.h .
+copy ..\build.vc10\getrusage.h .
+copy ..\build.vc10\gettimeofday.h .
+
+cd speed
+cl %OPT% /c ..\..\tests\misc.c /I..\..
+cl %OPT% /c ..\..\tests\memory.c /I..\..
+cl %OPT% /c ..\..\tests\refmpn.c /I..\..
+cl %OPT% /c ..\..\build.vc10\win_timing.c
+cl %OPT% /c ..\..\build.vc10\getopt.c /I..\..
+for %%X in (..\..\tune\common.c ..\..\tune\mod_1_div.c ..\..\tune\set_strb.c ..\..\tune\divrem1div.c ..\..\tune\gcd_bin.c ..\..\tune\mod_1_inv.c ..\..\tune\divrem1inv.c ..\..\tune\gcdextod.c ..\..\tune\modlinv.c ..\..\tune\set_strs.c ..\..\tune\divrem2div.c ..\..\tune\gcdextos.c ..\..\tune\noop.c ..\..\tune\divrem2inv.c ..\..\tune\jacbase1.c ..\..\tune\powm_mod.c ..\..\tune\fac_ui_large.c ..\..\tune\jacbase2.c  ..\..\tune\powm_redc.c    ..\..\tune\fac_ui_small.c  ..\..\tune\jacbase3.c ..\..\tune\preinv_divrem_1.c) do (
+	cl -c %OPT% /I..\.. /I..\..\tests /I.. %%X
+)
+cl %OPT% /I..\.. /I.. /I..\..\tests ..\..\tune\speed.c *.obj ..\%MPIRLIB% advapi32.lib psapi.lib
+cd ..
+
+exit /b 0
+
+
+
+:tune
+
+md tune
+
+set OPT=%FLAGS% %FLAGS1%
+set MPIRLIB=
+if %LIBTYPE% == lib (set MPIRLIB=mpir.lib)
+
+copy ..\build.vc10\unistd.h .
+copy ..\build.vc10\getopt.h .
+copy ..\build.vc10\win_timing.h .
+
+cd tune
+cl %OPT% /c ..\..\tests\misc.c /I..\..
+cl %OPT% /c ..\..\tests\memory.c /I..\..
+cl %OPT% /c ..\..\tests\refmpn.c /I..\..
+cl %OPT% /c ..\..\build.vc10\win_timing.c
+cl %OPT% /c ..\..\build.vc10\getopt.c /I..\..
+for %%X in (..\..\tune\set_strp.c ..\..\tune\common.c ..\..\tune\mod_1_div.c ..\..\tune\set_strb.c ..\..\tune\divrem1div.c ..\..\tune\gcd_bin.c ..\..\tune\mod_1_inv.c ..\..\tune\divrem1inv.c ..\..\tune\gcdextod.c ..\..\tune\modlinv.c ..\..\tune\set_strs.c ..\..\tune\divrem2div.c ..\..\tune\gcdextos.c ..\..\tune\noop.c ..\..\tune\divrem2inv.c ..\..\tune\jacbase1.c ..\..\tune\powm_mod.c ..\..\tune\fac_ui_large.c ..\..\tune\jacbase2.c ..\..\tune\powm_redc.c ..\..\tune\fac_ui_small.c ..\..\tune\jacbase3.c ..\..\tune\preinv_divrem_1.c ..\..\build.vc10\tune\*.c) do (
+	cl -c %OPT% /I..\.. /I..\..\tests /I.. %%X
+)
+cl %OPT% /I..\.. /I.. /I..\..\tests ..\..\tune\tuneup.c *.obj ..\%MPIRLIB% advapi32.lib psapi.lib
+cd ..
+
+exit /b 0
+
+
