95bdc34c198daa8705f1c5af02d0c6f3129e706a
Brian Gladman <brg@gladman.plus.com>
2013-04-16 16:44:11 +0100

    add mingw/mingw64 inline assembler to Windows longlong_inc.h files

--- mpn/x86_64w/longlong_inc.h
+++ mpn/x86_64w/longlong_inc.h
@@ -1,48 +1,9 @@
-
-#ifdef _MSC_VER
-#  include <intrin.h>
-
-#define COUNT_LEADING_ZEROS_NEED_CLZ_TAB
-
-#  if defined( _WIN64 )
-
-#    pragma intrinsic(_BitScanForward64)
-#    pragma intrinsic(_BitScanReverse64)
-#    pragma intrinsic(_umul128)
-#    pragma intrinsic(_byteswap_uint64)
-
-#    define count_leading_zeros(c,x)        \
-      do { unsigned long _z;		    	\
-        ASSERT ((x) != 0);                  \
-        _BitScanReverse64(&_z, (x));        \
-        c = 63 - _z;                        \
-      } while (0)
-#    define count_trailing_zeros(c,x)       \
-      do { unsigned long _z;		    	\
-        ASSERT ((x) != 0);                  \
-        _BitScanForward64(&_z, (x));        \
-		c = _z;								\
-      } while (0)
-#    define umul_ppmm(xh, xl, m0, m1)       \
-      do {                                  \
-        xl = _umul128( (m0), (m1), &xh);    \
-      } while (0)
-
-#    if !defined( BSWAP_LIMB )
-#      define BSWAP_LIMB
-#      define BSWAP_LIMB(dst, src)  dst = _byteswap_uint64(src)
-#    endif
-
-#  endif    /* _WIN64 */
-
-#endif      /* _MSC_VER */
-
-/* We need to put the the gcc inline asm for MinGW64 here as well */
-
 /* longlong.h -- definitions for mixed size 32/64 bit arithmetic.
 
 Copyright 1991, 1992, 1993, 1994, 1996, 1997, 1999, 2000, 2001, 2002, 2003,
 2004, 2005 Free Software Foundation, Inc.
+
+Copyright 2013 William Hart
 
 This file is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
@@ -59,7 +20,7 @@ along with this file; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
-#if defined (__GNUC__) || defined(INTEL_COMPILER)
+#if defined (__GNUC__) || defined(INTEL_COMPILER)  || defined(__ICL)
 
 #define add_ssaaaa(sh, sl, ah, al, bh, bl) \
   __asm__ ("addq %5,%q1\n\tadcq %3,%q0"					\
@@ -79,6 +40,19 @@ MA 02110-1301, USA. */
   __asm__ ("divq %4"		     /* stringification in K&R C */	\
 	   : "=a" (q), "=d" (r)						\
 	   : "0" ((UDItype)(n0)), "1" ((UDItype)(n1)), "rm" ((UDItype)(dx)))
+#define add_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+  __asm__ ("addq %8,%q2\n\tadcq %6,%q1\n\tadcq %4,%q0"     \
+       : "=r" (sh), "=r" (sm), "=&r" (sl)                  \
+       : "0"  ((UDItype)(ah)), "rme" ((UDItype)(bh)),  \
+         "1"  ((UDItype)(am)), "rme" ((UDItype)(bm)),  \
+         "2"  ((UDItype)(al)), "rme" ((UDItype)(bl)))  
+#define sub_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+  __asm__ ("subq %8,%q2\n\tsbbq %6,%q1\n\tsbbq %4,%q0"     \
+       : "=r" (sh), "=r" (sm), "=&r" (sl)                  \
+       : "0"  ((UDItype)(ah)), "rme" ((UDItype)(bh)),  \
+         "1"  ((UDItype)(am)), "rme" ((UDItype)(bm)),  \
+         "2"  ((UDItype)(al)), "rme" ((UDItype)(bl)))  
+
 /* bsrq destination must be a 64-bit register, hence UDItype for __cbtmp. */
 #define count_leading_zeros(count, x)					\
   do {									\
@@ -95,12 +69,80 @@ MA 02110-1301, USA. */
     __asm__ ("bsfq %1,%q0" : "=r" (count) : "rm" ((UDItype)(x)));	\
   } while (0)
 
-#if !defined(BSWAP_LIMB)
-#define BSWAP_LIMB
-#define BSWAP_LIMB(dst, src)	\
-  do {							\
-    __asm__ ("bswap %q0" : "=r" (dst) : "0" (src));	\
+#endif
+
+#if !defined(BSWAP_LIMB) && defined (__GNUC__) 
+#define BSWAP_LIMB(dst, src)						\
+  do {									\
+    __asm__ ("bswap %q0" : "=r" (dst) : "0" (src));			\
   } while (0)
 #endif
 
+#if defined( _MSC_VER )
+#include <intrin.h>
+
+#if !defined( COUNT_LEADING_ZEROS_NEED_CLZ_TAB )
+#  define COUNT_LEADING_ZEROS_NEED_CLZ_TAB
 #endif
+
+#if defined( _WIN64 )
+
+#if !defined(count_leading_zeros)
+#   pragma intrinsic(_BitScanReverse64)
+#   define count_leading_zeros(c,x)	    \
+    do  { unsigned long _z;	            \
+          ASSERT ((x) != 0);            \
+          _BitScanReverse64(&_z, (x));  \
+          c = 63 - _z;                  \
+        } while (0)
+#endif
+
+#if !defined(count_trailing_zeros)
+#   pragma intrinsic(_BitScanForward64)
+#   define count_trailing_zeros(c,x)	\
+    do  {  unsigned long _z;		 	\
+           ASSERT ((x) != 0);           \
+           _BitScanForward64(&_z, (x));	\
+		   c = _z;						\
+         } while (0)
+#endif
+
+#if !defined(umul_ppmm)
+#    pragma intrinsic(_umul128)
+#    define umul_ppmm(xh, xl, m0, m1)       \
+     do  {                                  \
+           xl = _umul128( (m0), (m1), &xh); \
+         } while (0)
+#endif
+
+#if !defined( BSWAP_LIMB )
+#  pragma intrinsic(_byteswap_uint64)
+#    define BSWAP_LIMB
+#    define BSWAP_LIMB(dst, src)  dst = _byteswap_uint64(src)
+#endif
+
+#endif    /* _WIN64 */
+
+#if 0	/* trial re-definition of C versions */
+#define add_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+   do { \
+      UWtype _cy; \
+      (sl) = (al) + (bl); \
+	  _cy = (sl) < (bl) ? 1 : 0; \
+	  (sm) = (am) + (bm) + _cy; \
+	  _cy  = (sm) < (bm) ? 1 : (sm) > (bm) ? 0 : _cy; \
+	  (sh) = (ah) + (bh) + _cy; \
+   } while (0)
+
+#define sub_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+   do { \
+      UWtype _bw; \
+      (sl) = (al) - (bl); \
+	  _bw = (sl) > (al) ? 1 : 0; \
+	  (sm) = (am) - (bm) - _bw; \
+	  _bw = (sm) > (am) ? 1 : (sm) < (am) ? 0 : _bw; \
+	  (sh) = (ah) - (bh) - _bw; \
+   } while (0)
+#endif
+
+#endif      /* _MSC_VER */
--- mpn/x86w/longlong_inc.h
+++ mpn/x86w/longlong_inc.h
@@ -1,4 +1,181 @@
+/* longlong.h -- definitions for mixed size 32/64 bit arithmetic.
 
-#ifdef _MSC_VER
-#define COUNT_LEADING_ZEROS_NEED_CLZ_TAB
+Copyright 1991, 1992, 1993, 1994, 1996, 1997, 1999, 2000, 2001, 2002, 2003,
+2004, 2005 Free Software Foundation, Inc.
+
+Copyright 2013 William Hart
+
+This file is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+This file is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this file; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#if defined (__GNUC__) || defined(INTEL_COMPILER) || defined(__ICL)
+
+#define add_ssaaaa(sh, sl, ah, al, bh, bl) \
+  __asm__ ("addl %5,%k1\n\tadcl %3,%k0"					\
+	   : "=r" (sh), "=&r" (sl)					\
+	   : "0"  ((USItype)(ah)), "g" ((USItype)(bh)),			\
+	     "%1" ((USItype)(al)), "g" ((USItype)(bl)))
+#define sub_ddmmss(sh, sl, ah, al, bh, bl) \
+  __asm__ ("subl %5,%k1\n\tsbbl %3,%k0"					\
+	   : "=r" (sh), "=&r" (sl)					\
+	   : "0" ((USItype)(ah)), "g" ((USItype)(bh)),			\
+	     "1" ((USItype)(al)), "g" ((USItype)(bl)))
+#define add_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+  __asm__ ("addl %8,%q2\n\tadcl %6,%q1\n\tadcl %4,%q0"     \
+       : "=r" (sh), "=r" (sm), "=&r" (sl)                  \
+       : "0"  ((USItype)(ah)), "rme" ((USItype)(bh)),  \
+         "1"  ((USItype)(am)), "rme" ((USItype)(bm)),  \
+         "2"  ((USItype)(al)), "rme" ((USItype)(bl)))  
+#define sub_333(sh, sm, sl, ah, am, al, bh, bm, bl)  \
+  __asm__ ("subl %8,%q2\n\tsbbl %6,%q1\n\tsbbl %4,%q0"     \
+       : "=r" (sh), "=r" (sm), "=&r" (sl)                  \
+       : "0"  ((USItype)(ah)), "rme" ((USItype)(bh)),  \
+         "1"  ((USItype)(am)), "rme" ((USItype)(bm)),  \
+         "2"  ((USItype)(al)), "rme" ((USItype)(bl)))  
+#define umul_ppmm(w1, w0, u, v) \
+  __asm__ ("mull %3"							\
+	   : "=a" (w0), "=d" (w1)					\
+	   : "%0" ((USItype)(u)), "rm" ((USItype)(v)))
+#define udiv_qrnnd(q, r, n1, n0, dx) /* d renamed to dx avoiding "=d" */\
+  __asm__ ("divl %4"		     /* stringification in K&R C */	\
+	   : "=a" (q), "=d" (r)						\
+	   : "0" ((USItype)(n0)), "1" ((USItype)(n1)), "rm" ((USItype)(dx)))
+
+
+#if __GMP_GNUC_PREREQ (3,4)  /* using bsrl */
+#define count_leading_zeros(count,x)  count_leading_zeros_gcc_clz(count,x)
+#endif /* gcc clz */
+
+#ifndef count_leading_zeros
+#define count_leading_zeros(count, x)					\
+  do {									\
+    USItype __cbtmp;							\
+    ASSERT ((x) != 0);							\
+    __asm__ ("bsrl %1,%0" : "=r" (__cbtmp) : "rm" ((USItype)(x)));	\
+    (count) = __cbtmp ^ 31;						\
+  } while (0)
+#endif /* asm bsrl */
+
+#if __GMP_GNUC_PREREQ (3,4)  /* using bsfl */
+#define count_trailing_zeros(count,x)  count_trailing_zeros_gcc_ctz(count,x)
+#endif /* gcc ctz */
+
+#ifndef count_trailing_zeros
+#define count_trailing_zeros(count, x)					\
+  do {									\
+    ASSERT ((x) != 0);							\
+    __asm__ ("bsfl %1,%0" : "=r" (count) : "rm" ((USItype)(x)));	\
+  } while (0)
+#endif /* asm bsfl */
+
+
+/* ASM_L gives a local label for a gcc asm block, for use when temporary
+   local labels like "1:" might not be available, which is the case for
+   instance on the x86s (the SCO assembler doesn't support them).
+
+   The label generated is made unique by including "%=" which is a unique
+   number for each insn.  This ensures the same name can be used in multiple
+   asm blocks, perhaps via a macro.  Since jumps between asm blocks are not
+   allowed there's no need for a label to be usable outside a single
+   block.  */
+
+#define ASM_L(name)  LSYM_PREFIX "asm_%=_" #name
+
+#if ! WANT_ASSERT
+/* Better flags handling than the generic C gives on i386, saving a few
+   bytes of code and maybe a cycle or two.  */
+
+#define MPN_IORD_U(ptr, incr, aors)					\
+  do {									\
+    mp_ptr  __ptr_dummy;						\
+    if (__builtin_constant_p (incr) && (incr) == 1)			\
+      {									\
+        __asm__ __volatile__						\
+          ("\n" ASM_L(top) ":\n"					\
+           "\t" aors " $1, (%0)\n"					\
+           "\tleal 4(%0),%0\n"						\
+           "\tjc " ASM_L(top)						\
+           : "=r" (__ptr_dummy)						\
+           : "0"  (ptr)							\
+           : "memory");							\
+      }									\
+    else								\
+      {									\
+        __asm__ __volatile__						\
+          (   aors  " %2,(%0)\n"					\
+           "\tjnc " ASM_L(done) "\n"					\
+           ASM_L(top) ":\n"						\
+           "\t" aors " $1,4(%0)\n"					\
+           "\tleal 4(%0),%0\n"						\
+           "\tjc " ASM_L(top) "\n"					\
+           ASM_L(done) ":\n"						\
+           : "=r" (__ptr_dummy)						\
+           : "0"  (ptr),						\
+             "ri" (incr)						\
+           : "memory");							\
+      }									\
+  } while (0)
+
+#ifndef MPN_INCR_U
+#define MPN_INCR_U(ptr, size, incr)  MPN_IORD_U (ptr, incr, "addl")
+#endif
+#ifndef MPN_DECR_U
+#define MPN_DECR_U(ptr, size, incr)  MPN_IORD_U (ptr, incr, "subl")
+#endif
+#ifndef mpn_incr_u
+#define mpn_incr_u(ptr, incr)  MPN_INCR_U (ptr, 0, incr)
+#endif
+#ifndef mpn_decr_u
+#define mpn_decr_u(ptr, incr)  MPN_DECR_U (ptr, 0, incr)
+#endif
+#endif
+
+#endif 
+
+#if defined (__GNUC__)
+#if __GMP_GNUC_PREREQ (3,1)
+#define __GMP_qm "=Qm"
+#define __GMP_q "=Q"
+#else
+#define __GMP_qm "=qm"
+#define __GMP_q "=q"
+#endif
+#ifndef ULONG_PARITY
+#define ULONG_PARITY(p, n)						\
+  do {									\
+    char	   __p;							\
+    unsigned long  __n = (n);						\
+    __n ^= (__n >> 16);							\
+    __asm__ ("xorb %h1, %b1\n\t"					\
+	     "setpo %0"							\
+	 : __GMP_qm (__p), __GMP_q (__n)				\
+	 : "1" (__n));							\
+    (p) = __p;								\
+  } while (0)
+#endif
+#endif
+
+#if !defined(BSWAP_LIMB) && defined (__GNUC__)
+#define BSWAP_LIMB(dst, src)						\
+  do {									\
+    __asm__ ("bswap %0" : "=r" (dst) : "0" (src));			\
+  } while (0)
+#endif
+
+#if defined( _MSC_VER )
+#  if !deffined(COUNT_LEADING_ZEROS_NEED_CLZ_TAB)
+#    define COUNT_LEADING_ZEROS_NEED_CLZ_TAB
+#  endif
 #endif
