947fb0832ccc76de44c48b8d9909365c7836e839
Isuru Fernando <isuruf@gmail.com>
2016-03-31 16:02:59 +0530

    Appveyor for windows CI

--- build.vc14/msbuild.bat
+++ build.vc14/msbuild.bat
@@ -30,8 +30,7 @@ if /i "%libp%" == "LIB" (
 )
 
 if /i "%5" EQU "+tests" (
-  set TESTDIR=.\mpir-tests\
-  for /d %%d in (%TESTDIR%*) do (
+  for /d %%d in (mpir-tests\*) do (
     for %%f in (%%d\*.vcxproj) do (
       %msbdir%\msbuild.exe /property:SolutionDir=..\..\ /property:OutDir=..\..\%plat%\%conf%\ /p:Platform=%plat% /p:Configuration=%conf% %%f
     )
