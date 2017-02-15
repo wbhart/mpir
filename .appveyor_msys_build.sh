c5a483813e5cf0640ca645835d748475f09a492f
Jean-Pierre Flori <jean-pierre.flori@ssi.gouv.fr>
2017-02-15 15:06:24 +0000

    Let appveyor test static and shared libs.

--- .appveyor_msys_build.sh
+++ .appveyor_msys_build.sh
@@ -1,6 +1,6 @@
 export PATH=/c/msys64/mingw$ABI/bin:/c/projects/mpir/bin/:$PATH
 cd /c/projects/mpir
 ./autogen.sh
-./configure ABI=$ABI --with-system-yasm
+./configure ABI=$ABI $LIB
 make
 make check
