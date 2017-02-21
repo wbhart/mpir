04189e2fc2a45f170775136a57ffb63bc1bf4884
William Hart <goodwillhart@googlemail.com>
2017-02-21 10:30:29 +0000

    Added another Broadwell.

--- cpuid.c
+++ cpuid.c
@@ -180,6 +180,7 @@ CPUVEC_SETUP_x86_64;
           if (model == 63){ CPUIS(haswell);break;}
           if (model == 69){ CPUIS(haswell);break;}
           if (model == 70){ CPUIS(haswell);break;}
+          if (model == 71){ CPUIS(broadwell);break;}
           if (model == 78){ CPUIS(skylakeavx);break;}
           if (model == 79){ CPUIS(broadwell);break;}
           if (model == 94){
