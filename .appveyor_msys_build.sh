9292da04d1cc4353c158c72b524a096847fe5eaf
Simon Sobisch <simonsobisch@web.de>
2019-09-30 16:01:40 +0200

    AppVeyor build update (upload artifacts, cache yasm)
    .appveyor_msys_build.sh:
    * output build commands
    * added `make dist` to create artifacts
    * deactivated: use DISTCHECK_CONFIGURE_FLAGS to pass used flags for distcheck (--> as comment because it currently doesn't work, so kept make check + make dist)
    
    appveyor.yml:
    * added artifacts (TODO: get MPIR version from file instead of current "3.")
    * get additional packages via pacman during install stage (lzip,zip) for building artifacts
    * moved yasm-download to install stage
    * yasm-version is specified in the environment, downloaded via https and cached

--- .appveyor_msys_build.sh
+++ .appveyor_msys_build.sh
@@ -1,6 +1,15 @@
 export PATH=/c/msys64/mingw$ABI/bin:/c/projects/mpir/bin/:$PATH
 cd /c/projects/mpir
+echo && echo build: ./autogen.sh
 ./autogen.sh
+echo && echo build: ./configure ABI=$ABI $LIB
 ./configure ABI=$ABI $LIB
+echo && echo build: make
 make
+# should work but falsely requires texlive ?!?
+#echo && echo build: DISTCHECK_CONFIGURE_FLAGS="ABI=$ABI $LIB" make distcheck
+#DISTCHECK_CONFIGURE_FLAGS="ABI=$ABI $LIB" make distcheck
+echo && echo build: make check
 make check
+echo && echo build: make dist
+make dist
